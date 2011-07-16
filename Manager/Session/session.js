a2ebaa793403f89f3861d2581134aa71bb20ad28
habib.virji <habib.virji@gmail.com>
2011-07-16 08:46:10 +0100

    added session.js

--- Manager/Session/session.js
+++ Manager/Session/session.js
@@ -1,6 +1,4 @@
 var tls = require('tls');
-<<<<<<< HEAD
-=======
 var fs = require('fs');
 
 var options = {
@@ -12,4 +10,3 @@ tls.createServer(options, function(s){
 s.write("hello");
 s.pipe(s);
 }).listen(8000);
->>>>>>> newbranch
