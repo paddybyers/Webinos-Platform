debf0698ef27f2cfed937dae69e38b61e9e3ab7b
Habib Virji <habib.virji@samsung.com>
2011-07-18 13:10:27 +0100

    Added directory for external project inputs

--- Manager/Session/session.js
+++ Manager/Session/session.js
@@ -1,12 +1,36 @@
 var tls = require('tls');
 var fs = require('fs');
 
-var options = {
-key:fs.readFileSync('server-key.pem'),
-cert:fs.readFileSync('server-cert.pem')
+var server-options = 
+{
+	key: fs.readFileSync('server-key.pem'),
+	cert: fs.readFileSync('server-cert.pem')
 };
 
-tls.createServer(options, function(s){
-s.write("hello");
-s.pipe(s);
+var client-options =
+{
+	key: fs.readFileSync('client-key.pem'),
+	cert: fs.readFileSync('client-cert.perm'),
+};
+
+var dummy = this;
+tls.createServer(server-options, function(s){
+	s.write("hello");
+	s.pipe(s);
 }).listen(8000);
+
+tls.connect(443, url, options, function(){
+	dummy.connected(true);
+});
+
+dummy.socket.addListener('data', function(data){
+
+});
+
+dummy.socket.addListener('error', function(data){
+
+});
+
+dummy.socket.addListener('close', function(data){
+
+});
