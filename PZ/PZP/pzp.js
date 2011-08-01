8cce24ec87bf0e5157b6a34fcfc035ddeabd178e
Habib Virji <habib.virji@samsung.com>
2011-08-01 14:41:08 +0100

    Resolved error in pzp.js

--- PZ/PZP/pzp.js
+++ PZ/PZP/pzp.js
@@ -1,6 +1,3 @@
 var session = require('../../Manager/Session/session.js');
-var discovery = require('../../Manager/Discovery/discovery.js');
 
 session.startTLSClient();
-//discovery.handlePresence();
-//discovery.scanBluetooth();
