0f597fb0441377dfab112e2c998871c5ef9c9aa1
Habib Virji <habib.virji@samsung.com>
2011-08-02 10:26:24 +0100

    Added Certificate Generation code in session manager

--- Manager/Session/session.js
+++ Manager/Session/session.js
@@ -3,7 +3,7 @@ var net = require('net');
 var tls = require('tls');
 var crypto = require('crypto');
 var fs = require('fs');
-//var generator= require('./build/default/generator.node');
+var generator= require('./build/default/generator.node');
 
 var port = 8005;
 var servername = 'localhost';	
@@ -12,18 +12,22 @@ var clientdata = '';
 	
 exports.startTLSServer = function()
 {
-	// This generates the rsa private key which can be used for signing certificate and is part of credential
-	/*generator.genrsa(1024, 'server-key.pem', function(err)
+	generator.genrsa(1024, 'server-key.pem', function(err)
 	{
 		log(err);
-	});*/
-	var connections = 0; 
+	});
+	
+	generator.gencert(2, 0, 30, 'server-cert.pem', function(err)
+	{
+		log(err);
+	});
+	
 	var server = net.createServer(function(socket) 
 	{
-		connections++;
-		log('connection fd=' + socket.fd);
+		log('server: connection fd = ' + socket.fd);
 		var sslcontext = crypto.createCredentials({key: fs.readFileSync('server-key.pem').toString(), 
-													cert: fs.readFileSync('server-cert.pem').toString()});
+												   cert: fs.readFileSync('server-cert.pem').toString()});
+		
 		sslcontext.context.setCiphers('AES128-SHA:AES256-SHA');
 
 		var pair = tls.createSecurePair(sslcontext, true);
@@ -32,58 +36,48 @@ exports.startTLSServer = function()
 
 		pair.on('secure', function() 
 		{
-			log('connected+secure!');
-			pair.cleartext.write('hello\r\n');
-			log(pair.cleartext.getPeerCertificate());
-			log(pair.cleartext.getCipher());
+			log('server: connected secure : ' + socket.remoteAddress);
+			pair.cleartext.write('hello\r\n');			
 		});
 
 		pair.cleartext.on('data', function(data) 
 		{
-			log('read bytes ' + data.length);
+			log('server: read bytes = ' + data.length);			
 			pair.cleartext.write(data);
 		});
 
 		socket.on('end', function() 
 		{
-			log('socket end');
+			log('server: socket end');
 		});
 
 		pair.cleartext.on('error', function(err) 
 		{
-			log('got error: ');
-			log(err);
-		    log(err.stack);
+			log('server: cleartext error: ' + err + '\n' + err.stack);
 			socket.destroy();
 		});
 
 		pair.encrypted.on('error', function(err) 
 		{
-			log('encrypted error: ');
-			log(err);
-			log(err.stack);
+			log('server: encrypted error: ' + err + '\n' + err.stack);
 			socket.destroy();
 		});
 
 		socket.on('error', function(err) 
 		{
-			log('socket error: ');
-			log(err);
-			log(err.stack);
+			log('server: socket error: ' + err + '\n' + err.stack);
 			socket.destroy();
-		});
-
-		socket.on('close', function(err) 
-		{
-			log('socket closed');
 		});
 
 		pair.on('error', function(err) 
 		{
-			log('secure error: ');
-			log(err);
-			log(err.stack);
+			log('server: secure error: ' + err + '\n' + err.stack);
 			socket.destroy();
+		});
+		
+		socket.on('close', function(err) 
+		{
+			log('socket closed');
 		});
 	});	
 	server.listen(port,servername);
@@ -92,16 +86,21 @@ exports.startTLSServer = function()
 
 exports.startTLSClient = function()
 {
-	/*generator.genrsa(1024, 'client-key.pem', function(err)
+	generator.genrsa(1024, 'client-key.pem', function(err)
 	{
-	  log(err);
-	});	
-	*/
+		log(err);
+	});
+		
+	generator.gencert(2, 0, 30, 'client-cert.pem', function(err)
+	{
+		log(err);
+	});
+	
 	var s = new net.Stream();
-	var sslcontext = crypto.createCredentials(
-					{key: fs.readFileSync('client-key.pem'), 
-					cert: fs.readFileSync('client-cert.pem')});
-	sslcontext.context.setCiphers('AES128-SHA');
+	var sslcontext = crypto.createCredentials({key: fs.readFileSync('client-key.pem').toString(), 
+												cert: fs.readFileSync('client-cert.pem').toString()});
+	
+	sslcontext.context.setCiphers('AES128-SHA:AES256-SHA');
 
 	var pair = tls.createSecurePair(sslcontext, false);
 	pair.encrypted.pipe(s);
@@ -111,41 +110,46 @@ exports.startTLSClient = function()
 
 	s.on('connect', function() 
 	{
-		log('client connected');
+		log('client: connected');
 	});
 
 	pair.on('secure', function() 
 	{
-		log('client: connected+secure!');
-		log('client pair.cleartext.getPeerCertificate(): %j',
-                pair.cleartext.getPeerCertificate());
-		log('client pair.cleartext.getCipher(): %j',
-                pair.cleartext.getCipher());
-		setTimeout(function() {
-			pair.cleartext.write('hello\r\n', function () {
-			gotWriteCallback = true;
-		});
+		log('client: connected secure');
+		log('client: pair.cleartext.getPeerCertificate(): %j', pair.cleartext.getPeerCertificate());
+		log('client: pair.cleartext.getCipher(): %j', pair.cleartext.getCipher());
+		
+		setTimeout(function() 
+		{
+			pair.cleartext.write('hello\r\n', function () 
+			{
+				gotWriteCallback = true;
+			});
 		}, 500);
 	});
 
 	pair.cleartext.on('data', function(d) 
 	{
-		log('cleartext: %s', d.toString());
+		log('client: cleartext: %s', d.toString());
 	});
 
-	s.on('close', function() {
-		log('client close');
+	s.on('close', function() 
+	{
+		log('client: close');
 	});
 
-	pair.encrypted.on('error', function(err) {
-		log('encrypted error: ' + err);
+	pair.encrypted.on('error', function(err) 
+	{
+		log('client: encrypted error: ' + err);
 	});
 
-	s.on('error', function(err) {
-		log('socket error: ' + err);
+	s.on('error', function(err) 
+	{
+		log('client: socket error: ' + err);
 	});
 
-	pair.on('error', function(err) {
-		log('secure error: ' + err);
+	pair.on('error', function(err) 
+	{
+		log('client: secure error: ' + err);
 	});	
 }
