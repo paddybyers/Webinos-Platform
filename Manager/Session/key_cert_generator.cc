258892a2f08584271e9214fbf0f17c5acc48f9f5
Habib Virji <habib.virji@samsung.com>
2011-08-17 14:33:41 +0100

    Session Manager Certificate Handling

--- Manager/Session/key_cert_generator.cc
+++ Manager/Session/key_cert_generator.cc
@@ -1,8 +1,7 @@
-// code to generate functionality similar to genrsa
-
-// This will call genrsa and get the result back
-
-// The need for this code is to allow pzh and pzp to generate their own certificate
+/*
+*
+* 
+*/
 
 #include <node.h>
 #include <v8.h>
@@ -22,228 +21,299 @@
 #include <net/if.h>
 #include <sys/ioctl.h>
 #include <arpa/inet.h>
+#include <fstream>
+
+#include <stdio.h>
 
 using namespace node;
 using namespace v8;
-static RSA *rsa;
+
 static Persistent<String> errno_symbol;
 static Persistent<String> mac_symbol;
-static char mac[32];
+RSA *rsa; 
+char mac[32];
 
 class KeyGenerator:ObjectWrap 
 {
- public:
- KeyGenerator()
- {	
+	public:
+		static void Initialize(Handle<Object> target); 
+		static void Init(Handle<Object> target); 
+
+		KeyGenerator()
+		{	
+			rsa=NULL; 
+		}
+		
+		~KeyGenerator()
+		{
+			RSA_free(rsa);
+		}
+	protected:
+		static Handle<Value> GenRsa(const Arguments& args); 
+		static Handle<Value> GenCert(const Arguments& args);
+		static Handle<Value> GenClientCert(const Arguments& args);
+		static void get_mac_addr();
+		static Handle<Value> GetDeviceId(const Arguments& args);
+};
+
+
+void KeyGenerator::Init(Handle<Object> target) 
+{
+	HandleScope scope; 
+	errno_symbol = NODE_PSYMBOL("errno"); 
+	mac_symbol = NODE_PSYMBOL("mac"); 
+	KeyGenerator::Initialize(target);
+}
  
- }
- ~KeyGenerator()
- {
-	RSA_free(rsa);
- }
- static void InitKg(Handle<Object> target) 
- {
-   HandleScope scope; 
-   errno_symbol = NODE_PSYMBOL("errno"); 
-   mac_symbol = NODE_PSYMBOL("mac"); 
-   KeyGenerator::Initialize(target);
- }
- 
- static void Initialize(Handle<Object> target) 
- {
+void KeyGenerator::Initialize(Handle<Object> target) 
+{
    HandleScope scope;
    NODE_SET_METHOD(target, "genrsa", GenRsa);
    NODE_SET_METHOD(target, "gencert", GenCert);
    NODE_SET_METHOD(target, "genclientcert", GenClientCert);
    NODE_SET_METHOD(target, "getdeviceid", GetDeviceId);        
- }
+}
  
- static Handle<Value> GetDeviceId(const Arguments& args)
- {
+Handle<Value> KeyGenerator::GetDeviceId(const Arguments& args)
+{
 	HandleScope scope;
-	Local<Object> info = Object::New();
 	get_mac_addr();
+	Local<Object> info = Object::New();
 	info->Set(mac_symbol, v8::String::New(mac));
 	return scope.Close(info);
  }
  
-static Handle<Value> GenRsa(const Arguments& args) 
+ /*
+ * This functions generates RSA private key.
+ * Input Parameters
+ *	bits: 
+ *  file-name: 
+ * RSA value generated in this method is used in certificate signing
+ */
+Handle<Value> KeyGenerator::GenRsa(const Arguments& args) 
 {
-  HandleScope scope;
-  BIO *bio_err = BIO_new_fp(stderr, BIO_NOCLOSE|BIO_FP_TEXT);
-  BIO *out = BIO_new(BIO_s_file());
-  
-  if (args.Length() < 2 )
-  {
-    return ThrowException(Exception::TypeError(String::New("Bad argument")));
-  }
-	
-  int bits(args[0]->Uint32Value());
-  String::Utf8Value outfile(args[1]->ToString());
-  std::cerr << bits << "\t" << *outfile << std::endl;  
-  
-  Local<Object> info = Object::New();
-  
-  if (BIO_write_filename(out,*outfile) <= 0) 
-	return ThrowException(ErrnoException(errno));
+	HandleScope scope;
+	BIO *out = BIO_new(BIO_s_file());
+	BIGNUM *bn;
+	Local<Object> info = Object::New();
+    
+	//Input Parameters
+	int bits(args[0]->Uint32Value());
+	String::Utf8Value outfile(args[1]->ToString());
+ 
+	if (BIO_write_filename(out,*outfile) <= 0) 
+		return ThrowException(Exception::TypeError(String::New("Writing into file error")));
    
-  rsa = RSA_new();
-  if(!rsa) 
-	return ThrowException(ErrnoException(errno));
-  BIGNUM *bn = BN_new();
-  
-  if(!bn) 
-	return ThrowException(ErrnoException(errno));
-  int ret = BN_set_word(bn, RSA_F4); 
-  
-  if (!ret) 
-	return ThrowException(ErrnoException(errno)); 
+	if(!(rsa = RSA_new()))
+		return ThrowException(Exception::TypeError(String::New("Error initializing rsa")));
     
-  ret = RSA_generate_key_ex(rsa, bits, bn, NULL );
-  if (!ret) 
-	return ThrowException(ErrnoException(errno));
+	if(!(bn = BN_new())) 
+		return ThrowException(Exception::TypeError(String::New("Error initializing bn")));
+  
+	if (!BN_set_word(bn, RSA_F4)) 
+		return ThrowException(Exception::TypeError(String::New("Error during bn set word"))); 
     
-  if(!PEM_write_bio_RSAPrivateKey(out,rsa, NULL, NULL, 0,NULL, NULL)) 
-    return ThrowException(ErrnoException(errno));
+	if (!RSA_generate_key_ex(rsa, bits, bn, NULL )) 
+		return ThrowException(Exception::TypeError(String::New("Error during generate key ex")));
+    
+	if(!PEM_write_bio_RSAPrivateKey(out,rsa, NULL, NULL, 0,NULL, NULL)) 
+		return ThrowException(Exception::TypeError(String::New("Error writing private key file")));
 
-//  info->Set(name_symbol, String::New("Value"));
-  BIO_free_all(out);
-  BIO_free_all(bio_err);
-  BN_free(bn);		
-  return scope.Close(info);  
+	BIO_free_all(out);
+	BN_free(bn);		
+	return args.This();  
 }
 
-static Handle<Value> GenClientCert(const Arguments& args)
+Handle<Value> KeyGenerator::GenCert(const Arguments& args)
 {
-  HandleScope scope;
-  X509 *x509 = X509_new();
-  BIO *bio_err = BIO_new_fp(stderr, BIO_NOCLOSE);
-  EVP_PKEY *pkey = EVP_PKEY_new(); 
-  X509_NAME *name = X509_NAME_new();
-  X509_EXTENSION *ex;
-  BIGNUM *rand;
-  BIO *out = BIO_new(BIO_s_file());
-  ASN1_INTEGER *sno = NULL;
-  sno = ASN1_INTEGER_new();
-  rand = BN_new();
-  int validupto(args[0]->Uint32Value());
-  String::Utf8Value outfile(args[1]->ToString());
-  String::Utf8Value deviceid(args[2]->ToString());
-  
-  //String::Utf8Value interface(args[3]->ToString());
-  std::cerr<<"1";
-  if (BIO_write_filename(out,*outfile) <= 0) 
-	return ThrowException(Exception::TypeError(String::New("Output File Writing Error")));
-  BN_pseudo_rand(rand, 64, 0 , 0);
-  BN_to_ASN1_INTEGER(rand, sno);  	
-  EVP_PKEY_assign_RSA(pkey, rsa);
-  X509_set_version(x509, 2);
-  X509_set_serialNumber(x509, sno);
-  name = X509_get_subject_name(x509);
-  X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC, (const unsigned char*)"UK", -1, -1, 0); 
-  X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (const unsigned char*)"webinos", -1, -1, 0); 
-  X509_set_issuer_name(x509, name);
-  X509_set_subject_name(x509, name);
-  X509_set_pubkey(x509, pkey);
-  X509_gmtime_adj(X509_get_notBefore(x509),0);
-  X509_gmtime_adj(X509_get_notAfter(x509),(long)60*60*24*validupto);
-  ex = X509V3_EXT_conf_nid(NULL, NULL, NID_netscape_cert_type, (char*) "client");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);
-  ex = X509V3_EXT_conf_nid(NULL, NULL, NID_netscape_comment, (char*) "PZH generated certificate for client");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);
-  ex = X509V3_EXT_conf_nid(NULL, NULL, NID_netscape_ssl_server_name, (char*) "www.webinos.org");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);
-  std::cerr<< "Before Signing" << std::endl;
-  // Cerificate Signature + Algorithm
-  int ret = X509_sign(x509,pkey,EVP_sha1());
-  std::cerr<< "Signing Error : %d " << ret << std::endl;
-  if(!ret)
-	return ThrowException(Exception::TypeError(String::New("Signing Error")));
-  // Writes into the file
-  PEM_write_bio_X509(out, x509);
-  
-  // Free allocate stuff
-  EVP_PKEY_free(pkey);
-  X509_free(x509);
-  BN_free(rand);
-  BIO_free_all(out);
-  BIO_free(bio_err);
-  ASN1_INTEGER_free(sno);
-  return args.This();;
-}
-static Handle<Value> GenCert(const Arguments& args)
-{
-  HandleScope scope;
-  X509 *x509 = X509_new();
-  BIO *bio_err = BIO_new_fp(stderr, BIO_NOCLOSE);
-  EVP_PKEY *pkey = EVP_PKEY_new(); 
-  X509_NAME *name = X509_NAME_new();
-  X509_EXTENSION *ex;
-  BIGNUM *rand;
-  BIO *out = BIO_new(BIO_s_file());
-  ASN1_INTEGER *sno = NULL;
-  sno = ASN1_INTEGER_new();
-  rand = BN_new();
-  int validupto(args[0]->Uint32Value());
-  String::Utf8Value outfile(args[1]->ToString());
-  get_mac_addr();	
+	HandleScope scope;
+	X509 *x509;
+	EVP_PKEY *pkey;
+	X509_NAME *name;
+    BIO *out = BIO_new(BIO_s_file());
+    ASN1_INTEGER *sno;
+	BIGNUM *rand; 
 
-  if (BIO_write_filename(out,*outfile) <= 0) 
-	return ThrowException(Exception::TypeError(String::New("Output File Writing Error")));
-  BN_pseudo_rand(rand, 64, 0 , 0);
-  BN_to_ASN1_INTEGER(rand, sno);  	
-  EVP_PKEY_assign_RSA(pkey, rsa);
-  X509_set_version(x509, 2);
-  X509_set_serialNumber(x509, sno);
-  name = X509_get_subject_name(x509);
-  X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC, (const unsigned char*)"UK", -1, -1, 0); 
-  X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (const unsigned char*)"webinos", -1, -1, 0); 
-  X509_set_issuer_name(x509, name);
-  X509_set_subject_name(x509, name);
-  X509_set_pubkey(x509, pkey);
-  X509_gmtime_adj(X509_get_notBefore(x509),0);
-  X509_gmtime_adj(X509_get_notAfter(x509),(long)60*60*24*validupto);
-  ex = X509V3_EXT_conf_nid(NULL, NULL, NID_netscape_cert_type, (char*) "server");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);
-  ex = X509V3_EXT_conf_nid(NULL, NULL, NID_netscape_comment, (char*) "PZH self generated certificate");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);
-  ex = X509V3_EXT_conf_nid(NULL, NULL, NID_netscape_ssl_server_name, (char*) "www.webinos.org");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);
-  ex = X509V3_EXT_conf_nid(NULL, NULL, NID_basic_constraints, (char*)"critical,CA:TRUE");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);
-  /*ex = X509V3_EXT_conf_nid(NULL, NULL, NID_subject_key_identifier, (char*)"aes192");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);
-  std::cerr<<"18";*/
-  /*ex = X509V3_EXT_conf_nid(NULL, NULL, NID_authority_key_identifier, (char*)"keyid:always");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);*/
- /* std::cerr<<"19";	
-  ex = X509V3_EXT_conf_nid(NULL, NULL, NID_key_usage, (char*)"critical,keyCertSign,cRLSign");
-  X509_add_ext(x509,ex, -1);
-  X509_EXTENSION_free(ex);	*/	
-  // Cerificate Signature + Algorithm
-  int ret = X509_sign(x509,pkey,EVP_sha1());
-  if(!ret)
-	return ThrowException(Exception::TypeError(String::New("Signing Error")));
-  // Writes into the file
-  PEM_write_bio_X509(out, x509);
+	// Input parameters
+	String::Utf8Value country(args[0]->ToString());
+	String::Utf8Value state(args[1]->ToString());
+	String::Utf8Value city(args[2]->ToString());
+	String::Utf8Value organization(args[3]->ToString());
+	String::Utf8Value organizationUnit(args[4]->ToString());
+	String::Utf8Value commonname(args[5]->ToString());
+	String::Utf8Value email(args[6]->ToString());
+	int days(args[7]->Uint32Value());
+	String::Utf8Value deviceid(args[8]->ToString());
+	String::Utf8Value outfile(args[9]->ToString());
+    
+	if((x509 = X509_new()) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Mem alloc error")));
+
+	//Version 3 is hardcoded
+	if(!X509_set_version(x509, 0x2)) 
+  		return ThrowException(Exception::TypeError(String::New("Error setting version error")));
+	
+	//Should this be random or in sequence order??
+	sno = ASN1_INTEGER_new();
+	rand = BN_new();
+	BN_pseudo_rand(rand, 64, 0 , 0);
+	BN_to_ASN1_INTEGER(rand, sno);
+
+	if(!X509_set_serialNumber(x509, sno))
+	  	return ThrowException(Exception::TypeError(String::New("Error setting serial number")));
+	
+	if((name = X509_NAME_new()) == NULL)
+	 	return ThrowException(Exception::TypeError(String::New("Mem alloc error")));
+		
+	name = X509_get_subject_name(x509);
+	
+	X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC, (const unsigned char*)*country, -1, -1, 0); 
+	X509_NAME_add_entry_by_txt(name, "ST", MBSTRING_ASC, (const unsigned char*)*state, -1, -1, 0); 
+	X509_NAME_add_entry_by_txt(name, "L", MBSTRING_ASC, (const unsigned char*)*city, -1, -1, 0); 
+	X509_NAME_add_entry_by_txt(name, "O", MBSTRING_ASC, (const unsigned char*)*organization, -1, -1, 0); 
+	X509_NAME_add_entry_by_txt(name, "OU", MBSTRING_ASC, (const unsigned char*)*organizationUnit, -1, -1, 0); 
+
+	// We get information from user about common name we append information of common name with device id
+	// Result of below concatenation will be for example: PC:Deviceid@macaddress
+	Handle<String> name1 = String::NewSymbol(*commonname);       
+	if(strcmp(*deviceid,"null") == 0)
+	{
+		get_mac_addr();
+		Local<String> str = String::Concat(name1, String::New(":Deviceid@"));
+		Local<String> str1 = String::Concat(str->ToString(), String::NewSymbol(mac));  
+		String::Utf8Value str2(str1->ToString());
+		X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (const unsigned char*)*str2, -1, -1, 0); 
+	} 
+	else
+	{
+		Local<String> str= String::Concat(name1, String::NewSymbol(*deviceid)); 
+		String::Utf8Value str1(str->ToString());
+		X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (const unsigned char*)*str1, -1, -1, 0); 
+	}
   
-  // Free allocate stuff
-  EVP_PKEY_free(pkey);
-  X509_free(x509);
-  BN_free(rand);
-  BIO_free_all(out);
-  BIO_free(bio_err);
-  ASN1_INTEGER_free(sno);
-  return args.This();;
+	X509_NAME_add_entry_by_txt(name, "emailAddress", MBSTRING_ASC, (const unsigned char*)*email, -1, -1, 0); 
+	if(!X509_set_issuer_name(x509, name))
+	 	return ThrowException(Exception::TypeError(String::New("Error setting issuer name")));
+	if(!X509_set_subject_name(x509, name))
+  	 	return ThrowException(Exception::TypeError(String::New("Error setting subject name")));
+
+	// Certificate validity
+	if(X509_gmtime_adj(X509_get_notBefore(x509),0) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Error setting days in certificate")));	
+	if(X509_gmtime_adj(X509_get_notAfter(x509),(long)60*60*24*days) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Error setting days in certificate")));	
+
+	// Considering private key is available from above structure		
+	if((pkey = EVP_PKEY_new()) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Mem Alloc Error")));
+	EVP_PKEY_assign_RSA(pkey, rsa);
+	X509_set_pubkey(x509, pkey);
+
+	// Cerificate Signature + Algorithm
+	if(!X509_sign(x509,pkey,EVP_sha1()))
+		return ThrowException(Exception::TypeError(String::New("Signing Error")));
+	
+	// Write into the file
+	if (BIO_write_filename(out,*outfile) <= 0) 
+		return ThrowException(Exception::TypeError(String::New("Output Create File Error")));
+	if(!PEM_write_bio_X509(out, x509))
+		return ThrowException(Exception::TypeError(String::New("Output File Writing Error")));
+  
+	// Free allocated stuff
+	BN_free(rand);
+	EVP_PKEY_free(pkey);
+	X509_free(x509);
+	BIO_free_all(out);
+	ASN1_INTEGER_free(sno);
+	return args.This();;
+}
+
+Handle<Value> KeyGenerator::GenClientCert(const Arguments& args)
+{
+	HandleScope scope;
+	EVP_PKEY *pkey;
+	X509 *client, *server;
+	ASN1_INTEGER *sno;
+	BIO *bp;
+  	BIGNUM *rand; 
+
+	// Input parameters	
+	String::Utf8Value certInput(args[0]->ToString());
+	int days(args[1]->Uint32Value());
+	String::Utf8Value outfile(args[2]->ToString());
+  
+	// store in file
+	std::ofstream file;
+	file.open("client-cert.pem");
+	file << *certInput;
+	file.close();
+  
+	if((bp = BIO_new(BIO_s_file())) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Error creating bio_new")));
+    
+	// Reads CA certificate key and fetch private key
+	if (BIO_read_filename(bp,"server-key.pem") <= 0)
+		return ThrowException(Exception::TypeError(String::New("Error reading server key")));
+	if((pkey = PEM_read_bio_PrivateKey(bp,NULL, NULL, NULL)) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Error reading private key error")));
+
+	// Read CA certificate
+	if(BIO_read_filename(bp,"server-cert.pem") <= 0)
+		return ThrowException(Exception::TypeError(String::New("Error reading server key")));
+	if((server = PEM_read_bio_X509(bp,NULL, NULL, NULL)) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Server cert bio failed")));
+
+	// Read Client certificate
+	if(BIO_read_filename(bp,"client-cert.pem") <= 0)
+		return ThrowException(Exception::TypeError(String::New("Client read failed")));
+	if((client = PEM_read_bio_X509(bp,NULL, NULL, NULL)) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Client cert bio failed")));
+
+	// fetch Server Key
+	EVP_PKEY *upkey = X509_get_pubkey(server);
+	EVP_PKEY_copy_parameters(upkey,pkey);
+	EVP_PKEY_free(upkey);
+	if(pkey == NULL)
+		return ThrowException(Exception::TypeError(String::New("Error getting server private key")));
+
+  // This generates random number and writes into file ending with .srl
+    sno = ASN1_INTEGER_new();
+	rand = BN_new();
+	BN_pseudo_rand(rand, 64, 0 , 0);
+	BN_to_ASN1_INTEGER(rand, sno);
+
+  // TODO:To decide if this should this be random or in seq order	
+	if(!X509_set_serialNumber(client, sno))
+		return ThrowException(Exception::TypeError(String::New("Error setting serial number in certificate")));
+	 
+  // Set new issuer name based on this field certificate chain is checked
+	if(!X509_set_issuer_name(client, X509_get_subject_name(server)))
+		return ThrowException(Exception::TypeError(String::New("Error setting issuer name in certificate")));
+
+   // Certificate validity
+	if(X509_gmtime_adj(X509_get_notBefore(client),0) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Error setting days in certificate")));	
+	if(X509_gmtime_adj(X509_get_notAfter(client),(long)60*60*24*days) == NULL)
+		return ThrowException(Exception::TypeError(String::New("Error setting days in certificate")));	
+  
+  // Cerificate Signature
+	if(!X509_sign(client,pkey,EVP_sha1()))
+		return ThrowException(Exception::TypeError(String::New("Signing Error")));
+  
+  // Write certificate into file
+	if (BIO_write_filename(bp,*outfile) <= 0) 
+		return ThrowException(Exception::TypeError(String::New("Error reading/creating file")));
+	if(!PEM_write_bio_X509(bp, client))
+		return ThrowException(Exception::TypeError(String::New("Output File Writing Error")));
+
+  // Free allocated stuff
+	BIO_free_all(bp);  // Without this line client cert will not be written
+	EVP_PKEY_free(pkey);
+	X509_free(client);
+	X509_free(server);
+	ASN1_INTEGER_free(sno);
+	BN_free(rand);
+	return args.This();;
 }
 
 // To read mac address based on iface
@@ -252,33 +322,27 @@ static Handle<Value> GenCert(const Arguments& args)
 // Bluetooth: bnep0, pan0
 // Wifi: wlan0
 // Loopback: lo0
-
-static void get_mac_addr()
-{
-	
+void KeyGenerator::get_mac_addr()
+{	
 	struct ifreq ifr;
 	int sock;
 	sock = socket(PF_INET, SOCK_STREAM, 0);
 	snprintf(ifr.ifr_name, strlen("eth0")+1, "eth0");
-	
 	int ret = ioctl(sock, SIOCGIFHWADDR, &ifr);
-		
 	int j, k = 0;
-	
 	for(j = 0; j < 6; j++)
 	{				
 		k+=snprintf((mac)+k,sizeof(mac)-k-1, j ? ":%02x": "%02x", (int)(unsigned int)(unsigned char)(ifr.ifr_hwaddr.sa_data[j]));
-		//std::cerr<< mac << "\t" << k << "\t" << j << std::endl;			
 	}
 	std::cerr<< "MAC address @ " << mac << std::endl;	
 }
-};
+
 extern "C"
 {
 	static void init (Handle<Object> target)
 	{
-		KeyGenerator::InitKg(target);
+		KeyGenerator::Init(target);
 	}
 	
-	NODE_MODULE(generator, KeyGenerator::InitKg);
+	NODE_MODULE(generator, KeyGenerator::Init);
 }
--- Manager/Session/session.js
+++ Manager/Session/session.js
@@ -1,26 +1,64 @@
 var log = console.log;
-var net = require('net');
 var tls = require('tls');
-var crypto = require('crypto');
+var events = require('events');
 var fs = require('fs');
-//var generator= require('./build/default/generator.node');
+var generator= require('./build/default/generator.node');
 
 var port = 443;
-var servername = 'localhost';	
-var serverdata = '';
-var clientdata = '';
-	
-exports.startTLSServer = function()
+var servername = 'localhost';		
+
+function Server()
+{
+	this.status = 'notconn';
+}
+
+Server.prototype = new process.EventEmitter();
+
+Server.prototype.checkfiles = function()
 {
 // Create self signed certificate for PZH
 // openssl genrsa -out server-key.pem
 // openssl req -new -key server-key.pem -out server-csr.pem
-// openssl x509 -req -days 30 -in server-csr.pem -signkey server-key.pem -out server-cert.pem
-	
-	// Bits for key to be generated | KeyName
-	// generator.genrsa(1024, 'server-key.pem');
-	// Days|CertificateName|devicename|interface
-	// generator.gencert(30, 'server-cert.pem' );
+// openssl x509 -req -days 30 -in server-csr.pem -signkey server-key.pem -out server-cert.pem	
+	var self = this;
+	fs.readFile('server-key.pem', function(err)
+	{			
+		if(err)
+		{
+			log('server: generating server key');
+			// Bits for key to be generated | KeyName
+			generator.genrsa(1024, 'server-key.pem');
+			log('server: generating server cert');			
+			// This is correct way of doing, encapsulate values in object. This is some work as we need to call set function 
+			// separately to set all values see createCredentials which in turns calls SecureContext.  
+			var certinfo = 
+			{
+				country: 'UK',
+				state: 'Surrey',
+				city: 'Staines',
+				organization: 'Webinos',
+				organizationUnit: 'WP4',
+				common: 'pzh://dev.webinos.org',
+				email: 'internal@webinos.org',
+				days: 180
+				
+			};
+			// TODO: Temp work around, input should come from a file or through user
+			// Country, State, City, OrgName, OrgUnit, Common, Email, Days, DeviceId, CertificateName
+			generator.gencert('UK', 'Surrey', 'Staines', 'Webinos', 'WP4', 'pzh://dev.webinos.org', 
+							 'internal@webinos.org', 180, 'null', 'server-cert.pem' );
+			self.emit('checked','file created');		
+		}
+		else
+		{
+			self.emit('checked', 'file present');
+		}
+	});
+};
+
+Server.prototype.connect = function()
+{
+	var self = this;
 	
 	var options = 
 	{
@@ -30,19 +68,18 @@ exports.startTLSServer = function()
 		requestCert:true, // This field controls whether client certificate will be fetched
 		requestUnauthorized:false
 	};
-	
 	var server = tls.createServer(options, function(conn)
 	{ 
 		if(conn.authorized)
 		{
 			log("Authenticated " + conn.getPeerCertificate());
-			data = {'status':'Auth'};
+			data = {'status':'Auth','clientcert':'','servercert':''};
 			conn.write(JSON.stringify(data));
 		}
 		else
 		{
 			log("Not Authenticated " + conn.authorizationError);			
-			var data = {'status':'NotAuth'};
+			var data = {'status':'NotAuth','clientcert':'','servercert':''};
 			conn.write(JSON.stringify(data));			
 		}	
 		
@@ -54,7 +91,16 @@ exports.startTLSServer = function()
 		conn.on('data', function(data) 
 		{
 			// Generate client certificate
-			log('server: read bytes = ' + data.length);						
+			log('server: read bytes = ' + data.length);	
+			var parse = JSON.parse(data);
+			if(parse.clientcert)		
+			{
+				// If we could get this information from within key exchange in openssl, it would not require certificate
+				generator.genclientcert(parse.clientcert, 120, 'client-cert.pem', function(err){log(err);});
+				data={'status':'','clientcert':fs.readFileSync('client-cert.pem').toString(),
+								'servercert':fs.readFileSync('server-cert.pem').toString()};
+				conn.write(JSON.stringify(data));					
+			}
 		});
 			
 		conn.on('end', function() 
@@ -72,52 +118,107 @@ exports.startTLSServer = function()
 			log('server:' + err + ' error stack : ' + err.stack);
 		});
 		
-	});		
-	server.listen(port,servername);
-}
+	});	
+	return server;
+};
 
-exports.startTLSClient = function()
+exports.startTLSServer = function()
 {
-// openssl genrsa -out client-key.pem
-// openssl req -new -key client-key.pem -out client-csr.pem
-// openssl x509 -req -days 30 -in client-csr.pem -CA ../PZH/server-cert.pem	-CAkey ../PZH/server-key.pem -CAcreateserial -out client-cert.pem
-// cp ../PZH/server-cert.pem .
-// openssl verify -CAfile server-cert.pem client-cert.pem 
-
-	// Bits for key to be generated | KeyName
-	// generator.genrsa(1024, 'client-key.pem')
-	// Days|CertificateName|devicename|interface
-	//generator.gencert(30, 'client-cert.pem' );
-	
-	var options = 
+	var server = new Server();	
+	server.on('checked',function(status)
 	{
-		key: fs.readFileSync('client-key.pem'),
-		cert: fs.readFileSync('client-cert.pem'),		
-		ca: fs.readFileSync('server-cert.pem'),	
-		requestCert:true
-	};
-
-	var client = tls.connect(port, options, function(conn1)
-	{
-		log('client: connect status: ' + client.authorized);
-		var conn = conn1;
-				
-		client.addListener('data',function(data)
-		{
-			parse = JSON.parse(data);
-			log('client: data received : ' + data);					
-			if (parse.status == 'NotAuth')
-			{
-				log('client: NotAuth');
-			}
-			else if (parse.status == 'Auth')
-			{
-				log('client: Auth : ');
-				log(client.getPeerCertificate());
-			}				
-		});		
+		log(status);
+		sock = server.connect();
+		sock.listen(port,servername);
 	});
 	
+	server.checkfiles();
+}	
+
+function Client()
+{
+	this.status = 'notconn';
+}
+
+Client.prototype = new process.EventEmitter();
+
+Client.prototype.checkfiles = function()
+{
+	var self = this;
+	fs.readFile('client-key.pem', function(err)
+	{	
+		// Bits for key to be generated | KeyName
+		if(err)
+		{
+			log('client: generating client key');
+			generator.genrsa(1024, 'client-key.pem');
+			
+			log('client: generating client cert');
+			generator.gencert('UK', 'Surrey', 'Staines', 'Webinos', 'WP4', 'PC', 'internal@webinos.org', 180,
+				'null', 'client-cert.pem' );		
+			var options1 = 
+			{
+				key: fs.readFileSync('client-key.pem'),		
+				cert: fs.readFileSync('client-cert.pem')				
+			};
+			self.emit('checked',options1);		
+		}
+		else
+		{
+			var options1 = 
+			{
+				key: fs.readFileSync('client-key.pem'),		
+				cert: fs.readFileSync('client-cert.pem'),
+				ca: fs.readFileSync('server-cert.pem')			
+			};
+			self.emit('checked', options1);
+		}
+	});
+};
+
+Client.prototype.connect = function(options)
+{
+	var self = this;
+
+	var client = tls.connect(port, options, function(conn)
+	{
+		log('client: connect status: ' + client.authorized);	
+	});
+	
+	client.on('data',function(data)
+	{
+		log('client: data received : ' + data);					
+		data1 = JSON.parse(data);
+		
+		if (data1.status == 'NotAuth')
+		{
+			log('client: NotAuth');
+			//mac = generator.getdeviceid();
+			var send = {'clientcert': fs.readFileSync('client-cert.pem').toString()};
+			log(send);
+			client.write(JSON.stringify(send));	
+		}
+		else if (data1.status == 'Auth')
+		{
+			log('client: Auth : ');
+			log(client.getPeerCertificate());
+		}		
+		
+		if(data1.clientcert!="")
+		{
+			log('creating client cert');
+			fs.writeFile('client-cert.pem', data1.clientcert);
+		}
+		
+		if(data1.servercert!="")
+		{
+			log('creating server cert');
+			fs.writeFile('server-cert.pem', data1.servercert);
+			self.emit('connect_again','connect');
+		}
+				
+	});		
+		
 	client.on('end',function()
 	{
 		log('client: Data End');		
@@ -125,6 +226,31 @@ exports.startTLSClient = function()
 	
 	client.on('close',function(data)
 	{
-		log('client:' + data);		
+		log('client: close ' + data);		
+	});	
+};
+ 
+exports.startTLSClient = function()
+{
+// openssl genrsa -out client-key.pem
+// openssl req -new -key client-key.pem -out client-csr.pem
+// openssl x509 -req -days 30 -in client-csr.pem -CA ../PZH/server-cert.pem	-CAkey ../PZH/server-key.pem -CAcreateserial -out client-cert.pem
+// cp ../PZH/server-cert.pem .
+// openssl verify -CAfile server-cert.pem client-cert.pem 
+	
+	var client = new Client();	
+	client.on('checked',function(status)
+	{
+		//log(status);
+		sock = client.connect(status);		
 	});
-}
+	
+	client.on('connect_again',function(status)
+	{
+		log(status);
+		client.checkfiles();
+	});
+	
+	client.checkfiles();	
+}	
+
