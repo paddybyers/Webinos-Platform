3d84ef6dec3c51fb208b63464a7f3eef1f41afa1
Habib Virji <habib.virji@samsung.com>
2011-09-08 17:39:30 +0100

    Task 613: Fetching client common name and session id in PZH and PZP

--- Manager/Session/session.js
+++ Manager/Session/session.js
@@ -2,25 +2,25 @@ var log = console.log;
 var tls = require('tls');
 var events = require('events');
 var fs = require('fs');
+var dns = require('dns');
+
+// This requires Manager/Session to be compiled before this file is available
 var generator= require('./build/default/generator.node');
 
+// Default port to be used
 var port = 443;
-var servername = 'localhost';		
 
-var client;
-var server;
-var config;
 
 function Server()
 {
-	this.status = 'notconn';
+    this.config = new Object();
+    this.connected_client = new Array();
 }
 
 Server.prototype = new process.EventEmitter();
 
 Server.prototype.readConfig = function()
 {
-	config = new Object();
 	var self = this;
 	fs.readFile('config.txt', function(err,data)
 	{
@@ -33,93 +33,124 @@ Server.prototype.readConfig = function()
 		for(var i=0; i<data1.length; i++)
 		{
 			if(data1[i][0] == 'country')
-				config.country = data1[i][1];
+				self.config.country = data1[i][1];
 			else if(data1[i][0] == 'state')
-				config.state = data1[i][1];
+				self.config.state = data1[i][1];
 			else if(data1[i][0] == 'city')
-				config.city = data1[i][1];
+				self.config.city = data1[i][1];
 			else if(data1[i][0] == 'organization')
-				config.orgname = data1[i][1];
+				self.config.orgname = data1[i][1];
 			else if(data1[i][0] == 'organizationUnit')
-				config.orgunit = data1[i][1];
+				self.config.orgunit = data1[i][1];
 			else if(data1[i][0] == 'common')
-				config.common = data1[i][1];
+				self.config.common = data1[i][1];
 			else if(data1[i][0] == 'email')
-				config.email = data1[i][1];
+				self.config.email = data1[i][1];
 			else if(data1[i][0] == 'days')
-				config.days = data1[i][1];
+				self.config.days = data1[i][1];
 			else if(data1[i][0] == 'keyName')
-				config.keyname = data1[i][1];
+				self.config.keyname = data1[i][1];
+            else if(data1[i][0] == 'keySize')
+				self.config.keysize = data1[i][1];
 			else if(data1[i][0] == 'certName')
-				config.certname = data1[i][1];
+				self.config.certname = data1[i][1];
+			else if(data1[i][0] == 'clientCertName')
+				self.config.clientcertname = data1[i][1];
+				
 		}
 		self.emit('configread','config read');		
 	});
 };
-
-Server.prototype.checkfiles = function()
-{
 // Create self signed certificate for PZH
 // openssl genrsa -out server-key.pem
 // openssl req -new -key server-key.pem -out server-csr.pem
 // openssl x509 -req -days 30 -in server-csr.pem -signkey server-key.pem -out server-cert.pem	
-
-	var self = this;
-
-	fs.readFile('server-key.pem', function(err)
-	{			
-		if(err)
-		{
-			self.readConfig();
-			self.on('configread',function()
-			{		
+Server.prototype.checkfiles = function()
+{
+	var self = this;	
+	self.readConfig();
+	self.on('configread',function()
+	{		
+		fs.readFile(self.config.keyname, function(err)
+		{			
+			if(err)
+			{			
 				log('server: generating server key');
 				// Bits for key to be generated | KeyName
-				generator.genrsa(1024, config.keyname);
+				generator.genrsa(self.config.keysize, self.config.keyname);
 				log('server: generating server cert');			
-				// Country, State, City, OrgName, OrgUnit, Common, Email, Days, DeviceId, CertificateName
-				generator.gencert(config.country, 
-						  config.state, 
-						  config.city, 
-						  config.orgname, 
-                                                  config.orgunit, 
-                                                  config.common,
-                                                  config.email, 
-                                                  config.days, 
-                                                  'null', 
-                                                  config.certname);
-				self.emit('checked','file created');		
-			});
-		}
-		else
-		{
-			self.emit('checked', 'file present');
-		}
+				// Country, State, City, OrgName, OrgUnit, Common, Email, Days, CertificateName
+				generator.gencert(self.config.country, 
+						self.config.state, 
+						self.config.city, 
+						self.config.orgname, 
+						self.config.orgunit, 
+						self.config.common,
+						self.config.email, 
+						self.config.days, 
+						self.config.certname);
+				self.emit('checked','file created');				
+			}
+			else
+			{
+				self.emit('checked', 'file present');
+			}
+		});
 	});
 };
 
 Server.prototype.connect = function()
 {
-	var options = 
+    var self = this;
+    var options = 
 	{
-		key: fs.readFileSync(config.keyname),
-		cert: fs.readFileSync(config.certname),
-		ca: fs.readFileSync(config.certname), // This is self signed certificate, so PZH is its own CA
-		requestCert:true, // This field controls whether client certificate will be fetched
+		key: fs.readFileSync(self.config.keyname),
+		cert: fs.readFileSync(self.config.certname),
+		ca: fs.readFileSync(self.config.certname), // This is self signed certificate, so PZH is its own CA
+		requestCert:true, // This field controls whether client certificate will be fetched for mutual authentication
 		requestUnauthorized:false
 	};
+
 	var server = tls.createServer(options, function(conn)
-	{ 
+	{    	    
 		if(conn.authorized)
 		{
-			log("Authenticated " + conn.getPeerCertificate());
-			data = {'status':'Auth','clientcert':'','servercert':''};
-			conn.write(JSON.stringify(data));
+			log("Authenticated ");
+			data = {'status':'Auth',
+				'clientcert':'',
+				'servercert':''};
+			// This is a session id created randomly of size 80 
+			// Each TLS connection in openssl has a session id but there accessing this id through node.js is not possible, so we create our own
+			var cn=conn.getPeerCertificate().subject.CN;
+			var found = false;
+			// This code is needed but for development purpose it is currently commented
+            /*for(var i=0; self.connected_client.length; i++)
+            {
+                if(self.connected_client[i] == cn)
+                    found = true;
+            }*/
+            if(found == false)
+            {    
+                var obj = new Object();
+			    obj.commonname=cn;
+                obj.sessionid=obj.commonname+':';
+                var temp = options.cert.toString();
+                for(var i=0; i< (80 - obj.commonname.length -1);i++)
+                {
+                    id =Math.floor(Math.random() * options.cert.length);
+                    obj.sessionid+=temp.substring(id, id+1);
+                }   
+                self.connected_client.push(obj);
+                log(JSON.stringify(self.connected_client));
+            }
+			conn.write(JSON.stringify(data));			
 		}
 		else
 		{
 			log("Not Authenticated " + conn.authorizationError);			
-			var data = {'status':'NotAuth','clientcert':'','servercert':''};
+			var data = {'status':'NotAuth',
+				    'clientcert':'',
+				    'servercert':''};
 			conn.write(JSON.stringify(data));			
 		}	
 		
@@ -136,9 +167,17 @@ Server.prototype.connect = function()
 			if(parse.clientcert)		
 			{
 				// If we could get this information from within key exchange in openssl, it would not require certificate
-				generator.genclientcert(parse.clientcert, config.days, 'client-cert.pem', function(err){log(err);});
-				data={'status':'','clientcert':fs.readFileSync('client-cert.pem').toString(),
-								'servercert':fs.readFileSync(config.certname).toString()};
+				generator.genclientcert(parse.clientcert, 
+							self.config.days, 
+							self.config.clientcertname, 
+							self.config.certname, 
+							function(err)
+							{
+								log(err);
+							});
+				data={'status':'',
+				      'clientcert':fs.readFileSync(self.config.clientcertname).toString(),
+				      'servercert':fs.readFileSync(self.config.certname).toString()};
 				conn.write(JSON.stringify(data));					
 			}
 		});
@@ -162,29 +201,29 @@ Server.prototype.connect = function()
 	return server;
 };
 
-exports.startTLSServer = function()
+exports.startTLSServer = function(arg)
 {
-	server = new Server();	
+	var server = new Server();	
 	server.on('checked',function(status)
 	{
 		log(status);
 		sock = server.connect();
-		sock.listen(port,servername);
-	});
-	
+		sock.listen(port,arg);		         
+		
+	});	
 	server.checkfiles();
 }	
 
 function Client()
 {
-	this.status = 'notconn';
+	this.config = new Object();
+    this.servername = new String();
 }
 
 Client.prototype = new process.EventEmitter();
 
 Client.prototype.readConfig = function()
 {
-	config = new Object();
 	var self = this;
 	fs.readFile('config.txt', function(err,data)
 	{
@@ -197,27 +236,29 @@ Client.prototype.readConfig = function()
 		for(var i=0; i<data1.length; i++)
 		{
 			if(data1[i][0] == 'country')
-				config.country = data1[i][1];
+				self.config.country = data1[i][1];
 			else if(data1[i][0] == 'state')
-				config.state = data1[i][1];
+				self.config.state = data1[i][1];
 			else if(data1[i][0] == 'city')
-				config.city = data1[i][1];
+				self.config.city = data1[i][1];
 			else if(data1[i][0] == 'organization')
-				config.orgname = data1[i][1];
+				self.config.orgname = data1[i][1];
 			else if(data1[i][0] == 'organizationUnit')
-				config.orgunit = data1[i][1];
+				self.config.orgunit = data1[i][1];
 			else if(data1[i][0] == 'common')
-				config.common = data1[i][1];
+				self.config.common = data1[i][1];
 			else if(data1[i][0] == 'email')
-				config.email = data1[i][1];
+				self.config.email = data1[i][1];
 			else if(data1[i][0] == 'days')
-				config.days = data1[i][1];
+				self.config.days = data1[i][1];
 			else if(data1[i][0] == 'keyName')
-				config.keyname = data1[i][1];
+				self.config.keyname = data1[i][1];
+			else if(data1[i][0] == 'keySize')
+				self.config.keysize = data1[i][1];
 			else if(data1[i][0] == 'certName')
-				config.certname = data1[i][1];
+				self.config.certname = data1[i][1];
 			else if(data1[i][0] == 'caName')
-				config.caname = data1[i][1];
+				self.config.caname = data1[i][1];
 		}
 		self.emit('configread','config read');		
 	});
@@ -226,34 +267,32 @@ Client.prototype.readConfig = function()
 Client.prototype.checkfiles = function()
 {
 	var self = this;
-	client.readConfig();			
+	self.readConfig();			
 	self.on('configread',function()
 	{
-		fs.readFile('client-key.pem', function(err)
+		fs.readFile(self.config.keyname, function(err)
 		{	
-			// Bits for key to be generated | KeyName
 			if(err)
 			{
 				log('client: generating client key');
-				generator.genrsa(1024, config.keyname);
+				// Bits for key to be generated | KeyName
+				generator.genrsa(self.config.keysize, self.config.keyname);
 			
 				log('client: generating client cert');
-				generator.gencert(config.country, 
-				  config.state, 
-				  config.city, 
-				  config.orgname, 
-                                  config.orgunit, 
-                                  config.common,
-                                  config.email, 
-                                  config.days, 
-                                  'null', 
-                                  config.certname);
-
+				generator.gencert(self.config.country, 
+					self.config.state, 
+					self.config.city, 
+					self.config.orgname, 
+					self.config.orgunit, 
+					self.config.common,
+					self.config.email, 
+					self.config.days, 
+					self.config.certname);
 			
 				var options1 = 
 				{
-					key: fs.readFileSync(config.keyname),		
-					cert: fs.readFileSync(config.certname)				
+					key: fs.readFileSync(self.config.keyname),		
+					cert: fs.readFileSync(self.config.certname)				
 				};
 				self.emit('checked',options1);		
 			}
@@ -261,9 +300,9 @@ Client.prototype.checkfiles = function()
 			{
 				var options1 = 
 				{
-					key: fs.readFileSync(config.keyname),		
-					cert: fs.readFileSync(config.certname),
-					ca: fs.readFileSync(config.caname)			
+					key: fs.readFileSync(self.config.keyname),		
+					cert: fs.readFileSync(self.config.certname),
+					ca: fs.readFileSync(self.config.caname)			
 				};
 				self.emit('checked', options1);
 			}
@@ -271,11 +310,11 @@ Client.prototype.checkfiles = function()
 	});
 };
 
-Client.prototype.connect = function(options)
+Client.prototype.connect = function(options, arg)
 {
 	var self = this;
 
-	var client = tls.connect(port, options, function(conn)
+	var client = tls.connect(port, arg, options, function(conn)
 	{
 		log('client: connect status: ' + client.authorized);	
 	});
@@ -283,32 +322,29 @@ Client.prototype.connect = function(options)
 	client.on('data',function(data)
 	{
 		log('client: data received : ' + data);					
-		data1 = JSON.parse(data);
-		
+		var data1 = JSON.parse(data);
 		if (data1.status == 'NotAuth')
 		{
 			log('client: NotAuth');
-			//mac = generator.getdeviceid();
-			var send = {'clientcert': fs.readFileSync(config.certname).toString()};
-			log(send);
+			var send = {'clientcert': fs.readFileSync(self.config.certname).toString()};
 			client.write(JSON.stringify(send));	
 		}
 		else if (data1.status == 'Auth')
 		{
-			log('client: Auth : ');
-			log(client.getPeerCertificate());
+			log('client: Authenticated');			
+			self.servername=client.getPeerCertificate().subject.CN;					
 		}		
 		
 		if(data1.clientcert!="")
 		{
 			log('creating client cert');
-			fs.writeFile(config.certname, data1.clientcert);
+			fs.writeFile(self.config.certname, data1.clientcert);
 		}
 		
 		if(data1.servercert!="")
 		{
 			log('creating server cert');
-			fs.writeFile('server-cert.pem', data1.servercert);
+			fs.writeFile(self.config.caname, data1.servercert);
 			self.emit('connect_again','connect');
 		}
 				
@@ -319,33 +355,32 @@ Client.prototype.connect = function(options)
 		log('client: Data End');		
 	});
 	
-	client.on('close',function(data)
+	client.on('close',function()
 	{
-		log('client: close ' + data);		
+		log('client: server close ');		
 	});	
 };
+
 // This function performs function equivalent to below commands
 // openssl genrsa -out client-key.pem
 // openssl req -new -key client-key.pem -out client-csr.pem
 // openssl x509 -req -days 30 -in client-csr.pem -CA ../PZH/server-cert.pem -CAkey ../PZH/server-key.pem -CAcreateserial -out client-cert.pem
 // cp ../PZH/server-cert.pem .
 // openssl verify -CAfile server-cert.pem client-cert.pem  
-exports.startTLSClient = function()
+exports.startTLSClient = function(arg)
 {
-	client = new Client();	
+	var client = new Client();	
 	client.on('checked',function(status)
 	{
-		sock = client.connect(status);		
+		log('client connecting');
+		sock = client.connect(status, arg);		
 	});
 	
 	client.on('connect_again',function(status)
 	{
-		log(status);
+		log('client connect again');
 		client.checkfiles();
 	});
 	
 	client.checkfiles();	
-}	
-
-
-
+}
