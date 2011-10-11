bc266b8a419c3cba6bb0da7c8475e9e2d9544aaf
Alexander Futász <alexander.futasz@fokus.fraunhofer.de>
2011-10-11 18:21:47 +0200

    Don't use for..in on arrays.

--- RPC/rpc_servicedisco.js
+++ RPC/rpc_servicedisco.js
@@ -3,9 +3,9 @@ webinos.rpc = require('./rpc.js');
 
 function findServices (params, successCB, errorCB, objectRef){
 	var serviceType = params[0];
-	var services = webinos.rpc.findServices(serviceType);
+	var services = webinos.rpc.findServices(serviceType) || [];
 	
-	for (var i in services) {
+	for (var i=0; i<services.length; i++) {
 		console.log('rpc.findService: calling found callback for ' + services[i].id);
 		var rpc = webinos.rpc.createRPC(objectRef, 'onservicefound', services[i]);
 		webinos.rpc.executeRPC(rpc);
