741f48a04049d62450927cec99c3552c389a2317
unknown <00917896@.telecomitalia.local>
2011-10-14 17:59:56 +0200

    First version of policy manager with nodejs-native interface.

---
+++ Manager/Policy/pm.cc
@@ -0,0 +1,91 @@
+
+#include <v8.h>
+#include <node.h>
+
+#include "core/policymanager/PolicyManager.h"
+
+using namespace node;
+using namespace v8;
+
+class PolicyManagerInt: ObjectWrap{
+
+private:  
+	int m_count;
+	
+public:
+	PolicyManager* pminst;
+	static Persistent<FunctionTemplate> s_ct;
+  
+	static void Init(Handle<Object> target)  {
+		HandleScope scope;
+		Local<FunctionTemplate> t = FunctionTemplate::New(New);
+		s_ct = Persistent<FunctionTemplate>::New(t);
+		s_ct->InstanceTemplate()->SetInternalFieldCount(1);
+		s_ct->SetClassName(String::NewSymbol("PolicyManagerInt"));
+		NODE_SET_PROTOTYPE_METHOD(s_ct, "checkFeature", CheckFeature);
+		target->Set(String::NewSymbol("PolicyManagerInt"),
+		s_ct->GetFunction());
+	}
+
+	PolicyManagerInt() :    m_count(0)  {
+	}
+	
+	~PolicyManagerInt()  {  }
+
+	static Handle<Value> New(const Arguments& args)  {
+		HandleScope scope;
+		PolicyManagerInt* pmtmp = new PolicyManagerInt();
+		PolicyManager* pminsttmp = new PolicyManager("./policy.xml");
+		pmtmp->pminst = pminsttmp;
+		pmtmp->Wrap(args.This());
+		return args.This();
+	}
+
+	static Handle<Value> CheckFeature(const Arguments& args)  {
+		HandleScope scope;
+
+		if (args.Length() < 1) {
+			return ThrowException(Exception::TypeError(String::New("Argument missing")));
+		}
+
+		if (!args[0]->IsString()) {
+			return ThrowException(Exception::TypeError(String::New("Bad type argument")));
+		}
+
+		v8::String::AsciiValue recFeature(args[0]);
+
+		PolicyManagerInt* pmtmp = ObjectWrap::Unwrap<PolicyManagerInt>(args.This());
+		pmtmp->m_count++;
+
+		string widPath(".");
+		
+		map<string, vector<string>*> * resource_attrs = new map<string, vector<string>*>();
+		(*resource_attrs)["api-feature"] = new vector<string>();
+		(*resource_attrs)["device-cap"] = new vector<string>();
+		(*resource_attrs)["device-cap"]->push_back(*recFeature);
+		
+		string roam("N");
+		map<string,string>* environment = new map<string,string>();
+		(*environment)["roaming"] = roam;
+		
+		Request* myReq = new Request(widPath, *resource_attrs, *environment);
+		
+		Effect myEff = pmtmp->pminst->checkRequest(myReq);
+
+		//enum Effect {PERMIT, DENY, PROMPT_ONESHOT, PROMPT_SESSION, PROMPT_BLANKET, UNDETERMINED, INAPPLICABLE};
+
+		Local<Integer> result = Integer::New(myEff);
+		
+		return scope.Close(result);
+	}
+};
+
+Persistent<FunctionTemplate> PolicyManagerInt::s_ct;
+
+extern "C" {
+	static void init (Handle<Object> target)  {
+		PolicyManagerInt::Init(target);  
+	}
+	NODE_MODULE(pm, init);
+} 
+
---
+++ Manager/Policy/pm.js
@@ -0,0 +1,80 @@
+
+var pmlib;
+var res;
+var pm;
+
+function runPolicyTest() {
+	try {
+		console.log("Loading policy module...");
+		pmlib = require('./build/default/pm.node');
+		
+		try {
+			pm = new pmlib.PolicyManagerInt();
+			console.log("Load success...");
+		}
+		catch(e) {
+			console.log("Load error: "+e.message);
+			return;
+		}
+
+		console.log("Calling checkFeature with no args...");
+		try {
+			res = pm.checkFeature();
+			console.log("Unexpected success...");
+		}
+		catch(e) {
+			console.log("Expected error: "+e.message+"...");
+		}
+		
+		console.log("Calling checkFeature with bad args...");
+		try {
+			res = pm.checkFeature(1);
+			console.log("Unexpected success...");
+		}
+		catch(e) {
+			console.log("Expected error: "+e.message+"...");
+		}
+		
+		console.log("Testing some features...");
+		
+		res = pm.checkFeature("web.contact");
+		console.log("web.contact: "+ruleEffectDescription(res));
+
+		res = pm.checkFeature("web.calendar");
+		console.log("web.calendar: "+ruleEffectDescription(res));
+
+		res = pm.checkFeature("web.contact.write");
+		console.log("web.contact.write: "+ruleEffectDescription(res));
+
+		res = pm.checkFeature("web.camera");
+		console.log("web.camera: "+ruleEffectDescription(res));
+
+		res = pm.checkFeature("web.calendar.write");
+		console.log("web.calendar.write: "+ruleEffectDescription(res));
+
+		res = pm.checkFeature("web.discovery");
+		console.log("web.discovery: "+ruleEffectDescription(res));
+
+	}
+	catch(e) {
+		console.log("error: "+e.message);
+	}
+}
+
+function ruleEffectDescription(num) {
+	if(num == 0)
+		return "PERMIT";
+	if(num == 1)
+		return "DENY";
+	if(num == 2)
+		return "PROMPT_ONESHOT";
+	if(num == 3)
+		return "PROMPT_SESSION";
+	if(num == 4)
+		return "PROMPT_BLANKET";
+	if(num == 5)
+		return "UNDETERMINED";
+	return "INAPPLICABLE";
+}
+
+runPolicyTest();
