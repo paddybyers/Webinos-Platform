a7b16c21c31dfe3548f923cfc15804450e638466
cntanos <cntanos@epu.ntua.gr>
2011-10-19 12:00:50 +0300

    Context data re-structuring

--- Manager/Context/Interception/contextInterception.js
+++ Manager/Context/Interception/contextInterception.js
@@ -12,7 +12,11 @@ if (typeof webinos.context === 'undefined')
 //This class represents the context objects that will be logged
 webinos.context.ContextData = function(method, params, results) {
 			this.timestamp = new Date();
-			this.method = method;
+		    var methodItm ={};
+		    methodItm.api = method.substring(0,method.indexOf("@"));
+		    methodItm.hash = method.substring(method.indexOf("@")+1,method.substring(method.indexOf("@")).indexOf(".")+ method.indexOf("@"));
+		    methodItm.method = method.substring(method.lastIndexOf(".")+1);
+			this.call = methodItm;
 			this.params = params;
 			this.results = results;
 		};
@@ -36,7 +40,7 @@ webinos.context.logContext = function(myObj, res) {
 			// Create the data object to log
 			var myData = new webinos.context.ContextData(myObj['method'],myObj['params'], res['result']);
 			
-			var dataIn = [{timestamp:myData.timestamp, method: myData.method, params:myData.params, result:myData.results}];
+			var dataIn = [{timestamp:myData.timestamp, call: myData.call, params:myData.params, result:myData.results}];
 			
 			webinos.context.database.insert(dataIn);
 			console.log("SAVED CONTEXT DATA");
