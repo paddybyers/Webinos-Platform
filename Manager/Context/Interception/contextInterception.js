8d73027b6293910a1ee76962d590c0689c975418
cntanos <cntanos@epu.ntua.gr>
2011-11-01 20:34:25 +0200

    Context: -The code for the secure storage in each transaction was removed. -The context interception module now stores a temporary file for up to the last 10 transactions and then it's contents are saved in the secure storage. This is only done to free up some resources from the constant encryptions of the zip file.

--- Manager/Context/Interception/contextInterception.js
+++ Manager/Context/Interception/contextInterception.js
@@ -25,14 +25,24 @@ var databasehelper = require('./../Storage/src/main/javascript/persist');
 
 // Initialize helper classes
 var pathclass = require('path');
-var fsclass = require('fs');
+var Fs = require('fs');
 
-var dbpath = pathclass
-.resolve('../Manager/Context/Storage/data/context.json');
+var dbpath = pathclass.resolve('../Manager/Context/Storage/data/context.json');
 console.log("CONTEXT DB Initialized");
+var logCount = 0;
+
+
+pathclass.exists(pathclass.dirname(dbpath) + "/temp", function (exists) {
+  if (! exists)
+  {
+    console.log("Context temp folder created")
+    Fs.mkdirSync(pathclass.dirname(dbpath) + "/temp", 0755);
+  }
+});
 
 // Open the database
-webinos.context.database = new databasehelper.JSONDatabase({path : dbpath,transactional : false});		
+webinos.context.database = new databasehelper.JSONDatabase({path : pathclass.dirname(dbpath) + "/temp/" + pathclass.basename(dbpath),transactional : false});
+webinos.context.tempDatabase = new databasehelper.JSONDatabase({path : dbpath,transactional : false});		
 
 webinos.context.logContext = function(myObj, res) {
 
@@ -42,12 +52,57 @@ webinos.context.logContext = function(myObj, res) {
 
   var dataIn = [{timestamp:myData.timestamp, call: myData.call, params:myData.params, result:myData.results}];
 
-  webinos.context.database.insert(dataIn);
-
+  
+  if(logCount == 10)
+  {
+    var securestore = require('../../Storage/src/main/javascript/securestore.js');
+    //var path = require('path');
+    var pass = "nruowgunrwognworu2";
+    //var pass = "";
+    
+    //open secure storage database
+    securestore.open(pass, dbpath + ".zip", pathclass.dirname(dbpath)
+        + "/temp", function() {
+      console.log("Encrypted Context file opened: " + dbpath + ".zip");
+      
+      webinos.context.tempDatabase.insert(dataIn);
+      
+      
+      webinos.context.database.insert(JSON.parse(Fs.readFileSync(dbpath, 'utf-8')));
+      webinos.context.tempDatabase.remove();
+      logCount =0;
+      securestore.close(pass, dbpath + ".zip", pathclass
+          .dirname(dbpath)
+          + "/temp", function() {
+        console.log("Encrypted Context file closed: " + dbpath + ".zip");
+        //Fs.writeFileSync(dbpath,"",'utf-8');
+        Fs.mkdirSync(pathclass.dirname(dbpath) + "/temp", 0755);
+        console.log("Secure Context Data Saved");
+      });
+    });
+  }
+  else
+  {
+    webinos.context.tempDatabase.insert(dataIn);
+    logCount +=1;
+    console.log("Temporary Context Data Saved");
+  }
   
   
+//Code to decrypt, unzip and re-encrypt the database for testing purposes
+  /*
+  securestore.decryptFile(that.config.path + ".zip", pass, function() {
+    securestore.unzipFile(that.config.path + ".zip", function() {
+      console.log("Unzipped");
+      securestore.encryptFile(that.config.path + ".zip", pass, function() {});
+    });
+    */
+  
+    
+  //webinos.context.database.insert(dataIn);
+  
+  //console.log(logCount);
   
   
-  console.log("SAVED CONTEXT DATA");
 };
 
--- Manager/Context/Storage/src/main/javascript/persist.js
+++ Manager/Context/Storage/src/main/javascript/persist.js
@@ -1,160 +1,113 @@
-/**
- * @constructor Create new JSONDatabase
- * 
- * @param {Object}
- *          [config] Optional configuration parameter.
- * @param {Object}
- *          [config.path] path to the database file.
- * @param {Object}
- *          [config.transactional] Set database in transactional mode. Requires
- *          commit of insert- and remove operation. Off by default.
- * @param {Object}
- *          [config.debug] Set database in debug mode. Off by default.
+/** 
+ * @constructor
+ * Create new JSONDatabase
+ *
+ * @param {Object} [config] Optional configuration parameter.
+ * @param {Object} [config.path] path to the database file.
+ * @param {Object} [config.transactional] Set database in transactional mode. 
+ *                                        Requires commit of insert- and remove operation. 
+ *                                        Off by default.
+ * @param {Object} [config.debug] Set database in debug mode. Off by default.
  */
 function JSONDatabase(config) {
-  "use strict";
-  var db_engine, param;
-  // Merge default configuration settings with provided config
-  this.config = {
-    path : './database.json',
-    transactional : false,
-    debug : false
-  };
+    "use strict";
+	var db_engine, param;
+    
+	// Merge default configuration settings with provided config
+	this.config = {path: './database.json', transactional: false, debug: false};
+    
+	for(param in  config) {
+		if(config.hasOwnProperty(param)) {
+			this.config[param] = config[param];
+		}
+	}
 
-  for (param in config) {
-    if (config.hasOwnProperty(param)) {
-      this.config[param] = config[param];
-    }
-  }
-
-  // Initialize JSORM database engine
-  db_engine = require('./../../lib/jsorm13/jsormdb-src');
-  this.db = db_engine.JSORM.db.db({
-    parser : db_engine.JSORM.db.parser.json(),
-    writeMode : db_engine.JSORM.db.db.modes.replace
-  });
-
-  // Read data file and insert contents in database engine
-  try {
-    this.db.insert(JSON.parse(require('fs').readFileSync(path.dirname(this.config.path) + "/temp/" + path.basename(this.config.path),
-        'utf-8')));
-  } catch (err) {
-  }
+	// Initialize JSORM database engine
+	db_engine = require('./../../lib/jsorm13/jsormdb-src');
+	this.db = db_engine.JSORM.db.db({parser: db_engine.JSORM.db.parser.json(), writeMode: db_engine.JSORM.db.db.modes.replace});
+	
+	// Read data file and insert contents in database engine
+	try{
+        this.db.insert(JSON.parse(require('fs').readFileSync(this.config.path, 'utf-8')));
+	} catch(err) {}
 }
 
 /**
  * Insert an arbitrary number of records into the database.
- * 
- * @param {Object[]}
- *          data. The records to be inserted, an array of JavaScript objects
+ *
+ * @param {Object[]} data. The records to be inserted, an array of JavaScript objects
  */
 JSONDatabase.prototype.insert = function(data) {
-  "use strict";
-  this.db.insert(data);
-
-  // in case no transaction support is required, commit changes immediately
-  if (!this.config.transactional) {
-    this.commit();
-  }
+    "use strict";
+	this.db.insert(data);
+	
+	// in case no transaction support is required, commit changes immediately
+	if(!this.config.transactional) {
+		this.commit();
+	}
 };
 
 /**
  * Remove records from the database.
  * 
- * @param {Object}
- *          query. Parameters for the removal.
- * @param {Object}
- *          [query.where] Search term, either primitive or composite, to
- *          determine which records to remove.
+ * @param {Object} query. Parameters for the removal.
+ * @param {Object} [query.where] Search term, either primitive or composite, to determine which records to remove.
  */
 JSONDatabase.prototype.remove = function(query) {
-  "use strict";
-  this.db.remove(query);
-
-  // in case no transaction support is required, commit changes immediately
-  if (!this.config.transactional) {
-    this.commit();
-  }
+    "use strict";
+	this.db.remove(query);
+	
+	// in case no transaction support is required, commit changes immediately
+	if(!this.config.transactional) {
+		this.commit();
+	}
 };
 
 /**
- * Search by query. Returns an array of records. No matches will return an empty
- * array; invalid query will return null.
- * 
- * @param {Object}
- *          query. Search parameters
- * @param {Object}
- *          query.where. Proper query term, either composite or primitive
- * @param {Object}
- *          [query.fields] Fields to return. This is an object literal. All
- *          fields that are set to non-null and have a match will return those
- *          fields. Returns all fields if null.
+ * Search by query. Returns an array of records. 
+ * No matches will return an empty array; invalid query will return null.
+ *
+ * @param {Object} query. Search parameters
+ * @param {Object} query.where. Proper query term, either composite or primitive
+ * @param {Object} [query.fields] Fields to return. This is an object literal. 
+ *                                All fields that are set to non-null and 
+ *                                have a match will return those fields. 
+ *                                Returns all fields if null.
  * @returns {Object[]} Array of the matched records
- */
+ */ 
 JSONDatabase.prototype.query = function(query) {
-  "use strict";
-  var results = this.db.find(query);
+    "use strict";
+	var results = this.db.find(query);
 
-  return results;
+	return results;
 };
 
 /**
- * Rollback a transaction. If given a count, it will reject the last count
- * activities. If given no count, a count of 0, or a count greater than the
- * total number of activities in this transaction, it will reject the entire
- * transaction.
+ * Rollback a transaction. If given a count, it will reject the last count activities. 
+ * If given no count, a count of 0, or a count greater than the total number of activities 
+ * in this transaction, it will reject the entire transaction.
  * 
- * @param {Integer}
- *          count. Number of steps within the transaction to reject. If empty,
- *          0, or greater than the total number of steps, the entire transaction
- *          will be rejected.
+ * @param {Integer} count. Number of steps within the transaction to reject. 
+ *                         If empty, 0, or greater than the total number of steps, 
+ *                         the entire transaction will be rejected.
  */
 JSONDatabase.prototype.rollback = function(count) {
-  "use strict";
-  // rollback changes to database since last commit, load, or db creation
-  this.db.reject();
+    "use strict";
+	// rollback changes to database since last commit, load, or db creation
+	this.db.reject();
 };
 
 /**
  * Commit the current transaction.
  */
 JSONDatabase.prototype.commit = function() {
-  "use strict";
-  var securestore = require('../../../../../Storage/src/main/javascript/securestore.js');
-  var path = require('path');
-  var pass = "nruowgunrwognworu2";
-  //var pass = "";
-  var that = this;
-  securestore.open(pass, that.config.path + ".zip", path.dirname(that.config.path)
-      + "/temp", function() {
-    console.log("Encrypted Context file opened: " + that.config.path + ".zip");
-    // commit changes to database since last commit, load, or db creation
-    that.db.commit();
-
-    // stringify content of database and write to filesystem
-    var content = JSON.stringify(that.db.find(), null, 4);
-    require('fs').writeFileSync(path.dirname(that.config.path) + "/temp/" + path.basename(that.config.path), content);
-    
-    securestore.close(pass, that.config.path + ".zip", path
-        .dirname(that.config.path)
-        + "/temp", function() {
-      console.log("Encrypted Context file closed: " + that.config.path + ".zip");
-      
-      //Code to decrypt, unzip and re-encrypt the database for testing purposes
-      /*
-      securestore.decryptFile(that.config.path + ".zip", pass, function() {
-        securestore.unzipFile(that.config.path + ".zip", function() {
-          console.log("Unzipped");
-          securestore.encryptFile(that.config.path + ".zip", pass, function() {});
-        });
-        */
-        
-      });
-      
-      
-    })
-  });
-
+    "use strict";
+	// commit changes to database since last commit, load, or db creation
+	this.db.commit();
+	
+	// stringify content of database and write to filesystem
+	var content = JSON.stringify(this.db.find(), null, 4);
+	require('fs').writeFileSync(this.config.path, content);
 };
 
 exports.JSONDatabase = JSONDatabase;
