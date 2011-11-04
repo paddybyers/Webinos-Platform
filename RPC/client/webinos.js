e4271e784b97c6b211249a2d582acb93d1046240
abot <abot@epu.ntua.gr>
2011-11-04 23:54:36 +0200

    Updated client webinos.js: It now detects the server host name instead of creating a websocket to localhost

--- RPC/client/webinos.js
+++ RPC/client/webinos.js
@@ -22,9 +22,9 @@
 	 */
 	function createCommChannel (successCB){
 		try{
-		channel  = new WebSocket('ws://127.0.0.1:8080');
+		channel  = new WebSocket('ws://'+window.location.hostname+':8080');
 		}catch(e){
-			channel  = new MozWebSocket('ws://127.0.0.1:8080');
+			channel  = new MozWebSocket('ws://'+window.location.hostname+':8080');
 		}
 		channel.onopen = function() {
 			webinos.rpc.setWriter(write);
@@ -88,6 +88,7 @@
 			if (typeof TVManager !== 'undefined') typeMap['http://webinos.org/api/tv'] = TVManager;
 			if (typeof DeviceStatusManager !== 'undefined') typeMap['http://wacapps.net/api/devicestatus'] = DeviceStatusManager;
 			if (typeof Contacts !== 'undefined') typeMap['http://www.w3.org/ns/api-perms/contacts'] = Contacts;
+			if (typeof BluetoothManager !== 'undefined') typeMap['http://webinos.org/manager/discovery/bluetooth'] = BluetoothManager;
 			// elevate baseServiceObj to usable local WebinosService object
 			
 			if (baseServiceObj.api === 'http://webinos.org/api/sensors.temperature'){
