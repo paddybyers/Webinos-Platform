f2c4835719efc0b3e86c2b61aee5af9325df3b18
abot <abot@epu.ntua.gr>
2011-11-06 00:21:22 +0200

    Review Demo: Added mobile TV Remote control (fireup an android firefox and go to demos/mobile/tvremote.html to change the channels)

---
+++ RPC/demos/mobile/client.js
@@ -0,0 +1,47 @@
+addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){
+window.scrollTo(0,1);
+}
+
+$(function() {
+if ($("ul.tabs")) $("ul.tabs").tabs("div.tabbed", {effect: 'ajax'});
+});
+
+$(function() { 
+if ($(".accordion")) $(".accordion").tabs(".accordion dd", {tabs: 'dt', effect: 'slide', initialIndex: null});
+});
+var tvService =null;
+var channelMap = {};
+$(function(){setTimeout(getServices,100);});
+
+function getServices(){
+  webinos.ServiceDiscovery.findServices(new ServiceType('http://webinos.org/api/tv'), {onFound: function (service) {
+		  tvService = service;
+		  tvService.tuner.getTVSources(/*TVSuccessCB*/ function(sources){
+		   var markup = "";
+		   jQuery.each( sources, function(indexInArray, source){
+		     markup +="<dt>"+ source.name + "</dt><dd>";
+		     jQuery.each( source.channelList, function(idxchn, channel){
+			markup += "<p><input name='"+source.name+'_'+channel.name+"' class='channelButton' type='button' value='"+ channel.name + "'/></p>";
+		        channelMap[source.name+'_'+channel.name] = channel;
+		     });
+		     markup +="</dd>";
+		   }); 
+		   if (markup != "") markup = "<dl class='accordion'>" + markup + "</dl>";
+		   $("#content").html(markup);
+		   if ($(".accordion"))
+		      $(".accordion").tabs(".accordion dd", {tabs: 'dt', effect: 'slide', initialIndex: null});
+		   if ($(".channelButton")){
+		     $(".channelButton").click(function (e) {
+			tvService.display.setChannel(channelMap[e.currentTarget.name],function(){$('#status').text("Channel changed to " + e.currentTarget.name); },function(){});
+		     });
+		   }
+		  }
+		  );
+	    }});
+  
+}
+
+$(document).ready(function() {
+if ($("#browsable")) $("#browsable").scrollable().navigator();	
+});
