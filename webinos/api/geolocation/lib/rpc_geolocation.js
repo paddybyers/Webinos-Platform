0d5bd855faf56289b1e1d30d0426499a643fec44
Simon Isenberg <simon.isenberg@bmw.de>
2011-12-05 13:37:05 +0100

    sarte integrate vehicle as a data provider for geolocation

--- webinos/api/geolocation/lib/rpc_geolocation.js
+++ webinos/api/geolocation/lib/rpc_geolocation.js
@@ -5,67 +5,122 @@ webinos.rpc = require('../../../common/rpc/lib/rpc.js');
 var watchIdTable = {};
 
 var counter = 0; // var used for debugging only;
+try{
+	var vehicleSystem = require('../../../../vehicle/src/main/javascript/cds_con/car.js');
+	var vehicleBusAvailable = vehicleSystem.available;
+	var car = vehicleSystem.most;
+}catch(e){
+	var vehicleBusAvailable = false;
+	var car = null;
+	console.log('vehicle bus connection module is not available');
+}
+/*
+var Position = function(timestamp, coords){
+	this.timestamp = timestamp;
+	this.coords = coords;
+}
+
+var Coordinates = function(latitude, longitude, altitude, accuracy, altitudeAccuracy, heading, speed){
+	this.latitude = latitude;
+	this.longitude = longitude;
+	this.altitude = altitude;
+	this.accuracy = accuracy;
+	this.altitudeAccuracy = altitudeAccuracy;
+	this.heading = heading
+	this.speed = speed;	
+}
+*/
 
 function getCurrentPosition (params, successCB, errorCB, objectRef){
 	var error = {};
-	var geoip = null;
-	var http = require('http');
-	var freegeoip = http.createClient(80, 'freegeoip.net');
-	var request = freegeoip.request('GET', '/json/', {'host': 'freegeoip.net'});
-	request.end();
-	request.on('response', function (response) {
-		// console.log('STATUS: ' + response.statusCode);
-		// console.log('HEADERS: ' + JSON.stringify(response.headers));
-		response.setEncoding('utf8');
-		response.on('data', function (chunk) {
-			console.log('geoip chunk: ' + chunk);
-			try { 
-				geoip = JSON.parse(chunk);
-			}
-			catch(err) {
-				error.code = 2; 
-				error.message = "failed getting IP address based geolocation";
-				console.log("error: " + JSON.stringify(error));
-				errorCB(error);
-				return;
-			}
-
-			var coords = new Object;
-			if (params) {
-				if (params.enableHighAccuracy) coords.accuracy = 1; else coords.accuracy = null; // simply reflect input for debugging
-			}
-			coords.altitude = counter++;
-			coords.altitudeAccuracy = null;
-			coords.heading = null;
-			coords.speed = Math.floor(Math.random()*1000)/10;
+	
+	if(vehicleBusAvailable){
 		
-			if (geoip) {
-				if (geoip.latitude) coords.latitude = geoip.latitude; else coords.latitude = null; 
-				if (geoip.longitude) coords.longitude = geoip.longitude; else coords.longitude = null; 
-			}	
-			var position = new Object;
-			position.coords=coords;
-			position.timestamp = new Date().getTime();
-			
-			if ((position.coords.latitude) && (position.coords.longitude)) {
-				successCB(position);
-				return;
-			}
-			else {
-				error.code = 2; 
-				error.message = "failed getting IP address based geolocation";
-				console.log("error: " + JSON.stringify(error));
-				errorCB(error);
-				return;
-			}
-	
-		});	 
-	});			
-	
+  		var position = new Object();
+  		
+  		d = new Date();
+  		position.timestamp = d.toUTCString(); //NEEDS BE CONVERTED TO UTC
+  		
+  		position.coords = new Object();
+  		
+  		position.coords.latitude = car.position.latitude.get() / Math.pow(2,32) * 360;
+  		position.coords.longitude = car.position.longitude.get() / Math.pow(2,32) * 360;
+		position.coords.accuracy = 99;
+		position.coords.heading = car.heading.get();
+		position.coords.speed = ((car.speed.get() / 10) / 3600) * 1000 ; // meters per second 
+ 		returnPosition(position, successCB, errorCB, objectRef);
+		return;
+		
+		
+	}else{
+		var geoip = null;
+		var http = require('http');
+		var freegeoip = http.createClient(80, 'freegeoip.net');
+		var request = freegeoip.request('GET', '/json/', {'host': 'freegeoip.net'});
+	 	request.end();
+	 	request.on('response', function (response) {
+		 // console.log('STATUS: ' + response.statusCode);
+		 // console.log('HEADERS: ' + JSON.stringify(response.headers));
+		 response.setEncoding('utf8');
+		 response.on('data', function (chunk) {
+			 console.log('geoip chunk: ' + chunk);
+			 try { 
+				 geoip = JSON.parse(chunk);
+			 }
+			 catch(err) {
+				 error.code = 2; 
+				 error.message = "failed getting IP address based geolocation";
+				 console.log("error: " + JSON.stringify(error));
+				 errorCB(error);
+				 return;
+			 }
+  
+			 var coords = new Object;
+			 if (params) {
+				 if (params.enableHighAccuracy) coords.accuracy = 1; else coords.accuracy = null; // simply reflect input for debugging
+			 }
+			 coords.altitude = counter++;
+			 coords.altitudeAccuracy = null;
+			 coords.heading = null;
+			 coords.speed = Math.floor(Math.random()*1000)/10;
+		 
+			 if (geoip) {
+				 if (geoip.latitude) coords.latitude = geoip.latitude; else coords.latitude = null; 
+				 if (geoip.longitude) coords.longitude = geoip.longitude; else coords.longitude = null; 
+			 }	
+			 var position = new Object;
+			 position.coords=coords;
+			 position.timestamp = new Date().getTime();
+			 
+			 if ((position.coords.latitude) && (position.coords.longitude)) {
+				 successCB(position);
+				 return;
+			 }
+			 else {
+				 error.code = 2; 
+				 error.message = "failed getting IP address based geolocation";
+				 console.log("error: " + JSON.stringify(error));
+				 errorCB(error);
+				 return;
+			 }
+	 
+		 });	 
+	 });			
+	}
 }
 
 function watchPosition (args, successCB, errorCB, objectRef) {
-	var tint = 2000;
+	
+    if(vehicleBusAvailable){
+		listeners.push([successCB, errorCB, objectRef]);	
+		if(!listeningToPosition){
+			car.position.bind(vehicleBusHandler);
+			listeningToPosition = true;
+		}
+		console.log(listeners.length + " listener(s) watching");
+	}else{
+    
+    var tint = 2000;
 	var params = args[0];
 	if (params.maximumAge) tint = params.maximumAge;
 	
@@ -88,6 +143,38 @@ function watchPosition (args, successCB, errorCB, objectRef) {
 	
 	var watchIdKey = args[1];
 	watchIdTable[watchIdKey] = watchId;
+    }
+}
+function returnPosition(position, successCB, errorCB, objectRef){
+	if(position === undefined){
+		errorCB('Position could not be retrieved');		
+	}else{
+		successCB(position);
+	}
+}
+
+var listeners = new Array();
+var listeningToPosition = false;
+
+function vehicleBusHandler(data){
+	
+	var position = new Object();
+  		
+  	d = new Date();
+  	position.timestamp = d.toUTCString(); //NEEDS BE CONVERTED TO UTC
+  		
+  	position.coords = new Object();
+  		
+  	position.coords.latitude = data.latitude / Math.pow(2,32) * 360;
+  	position.coords.longitude = data.longitude / Math.pow(2,32) * 360;
+	position.coords.accuracy = 99;
+	position.coords.heading = car.heading.get();
+	position.coords.speed = ((car.speed.get() / 10) / 3600) * 1000 ; // meters per second
+	
+	
+	for(var i = 0; i < listeners.length; i++){
+		returnPosition(position, function(e) {var rpc = webinos.rpc.createRPC(listeners[i][2], 'onEvent', [e]); webinos.rpc.executeRPC(rpc);}, listeners[i][1], listeners[i][2]);
+	}
 }
 
 function clearWatch (params, successCB, errorCB, objectRef) {
