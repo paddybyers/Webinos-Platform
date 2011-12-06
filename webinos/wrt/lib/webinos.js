18c0df4a88ce07e964dc6bf0fb0ec0b88031af31
Alexander Futasz <alexander.futasz@fokus.fraunhofer.de>
2011-12-06 12:13:00 +0100

    Split out the browser side code for geolocation.
    To its own file.

--- webinos/wrt/lib/webinos.js
+++ webinos/wrt/lib/webinos.js
@@ -670,48 +670,4 @@
     );
   };
   
- 
-	///////////////////// GEOLOCATION INTERFACE ///////////////////////////////
-	
-	var WebinosGeolocation;
-
-	WebinosGeolocation = function (obj) {
-		this.base = WebinosService;
-		this.base(obj);
-	};
-
-	WebinosGeolocation.prototype = new WebinosService;
-
-	WebinosGeolocation.prototype.getCurrentPosition = function (PositionCB, PositionErrorCB, PositionOptions) { 
-		var rpc = webinos.rpc.createRPC(this, "getCurrentPosition", PositionOptions); // RPC service name, function, position options
-		webinos.rpc.executeRPC(rpc, function (position) {
-			PositionCB(position); 
-		},
-		function (error) {
-			PositionErrorCB(error);
-		});
-	};
-
-	WebinosGeolocation.prototype.watchPosition = function (PositionCB, PositionErrorCB, PositionOptions) {
-		var watchIdKey = Math.floor(Math.random()*101);
-		
-		var rpc = webinos.rpc.createRPC(this, "watchPosition", [PositionOptions, watchIdKey]);
-		rpc.fromObjectRef = Math.floor(Math.random()*101); //random object ID	
-
-		var callback = new RPCWebinosService({api:rpc.fromObjectRef});
-		callback.onEvent = function (position) {
-			PositionCB(position); 
-		};
-		webinos.rpc.registerCallbackObject(callback);
-
-		webinos.rpc.executeRPC(rpc);
-
-		return watchIdKey;
-	};
-
-	WebinosGeolocation.prototype.clearWatch = function (watchId) {
-		var rpc = webinos.rpc.createRPC(this, "clearWatch", [watchId]); 
-		webinos.rpc.executeRPC(rpc, function() {}, function() {});
-	};
-	
 }());
