21a99efd4dd2608bf77e87b65eb2a87ab62b9cb6
abot <abot@epu.ntua.gr>
2011-12-07 16:25:33 +0200

    Fixed the webinos-gyp to use gyp from the node/tools/gyp directory instead of requiring it to be installed

--- Tools/gyp_tools/webinos-gyp.bat
+++ Tools/gyp_tools/webinos-gyp.bat
@@ -1,12 +1,11 @@
 @echo off
-@rem Check for gyp
-if not defined GYP_ROOT goto gyp-not-found
-if not exist "%GYP_ROOT%\gyp" goto gyp-not-found
 @rem Check for nodejs build location variable
 if not defined NODE_ROOT goto nodebuild-not-found
 if not exist "%NODE_ROOT%\src\node.h" goto nodebuild-not-found
 if not exist "%NODE_ROOT%\deps\v8\include\v8.h" goto nodebuild-not-found
 if not exist "%NODE_ROOT%\deps\uv\include\uv.h" goto nodebuild-not-found
+if not exist "%NODE_ROOT%\tools\gyp\gyp" goto gyp-not-found
+
 @rem detect the location of the node.lib file
 set node_lib_folder=
 if exist "%NODE_ROOT%\Release\node.lib" set node_lib_folder=Release
@@ -21,7 +20,7 @@ if exist %1 set gypfile=%1
 if not defined gypfile if exist "%CD%\module.gyp" set gypfile=module.gyp
 if not defined gypfile goto gyp-file-missing
 @rem Generate visual studio solution
-python %GYP_ROOT%\gyp -f msvs -G msvs_version=2010 %gypfile% --depth=. -DNODE_ROOT=%NODE_ROOT% -Dnode_lib_folder=%node_lib_folder%
+python %NODE_ROOT%\tools\gyp\gyp -f msvs -G msvs_version=2010 %gypfile% --depth=. -DNODE_ROOT=%NODE_ROOT% -Dnode_lib_folder=%node_lib_folder%
 if errorlevel 1 goto exit-error
 echo Finished
 goto exit
@@ -29,7 +28,7 @@ goto exit
 echo Visual studio tools were not found! Please check the VS100COMNTOOLS path variable
 goto exit
 :gyp-not-found
-echo GYP was not found. Please set the environment variable that points to the folder where the gyp executable is located
+echo GYP was not found. Please check that gyp is located in %NODE_ROOT%/tools/gyp/ 
 goto exit
 :nodebuild-not-found
 echo Node build path not found! Please check the NODE_ROOT path variable exists and that it points to the root of the git repo where you have build 
