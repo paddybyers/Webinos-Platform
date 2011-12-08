7de04dc353a20c290e5c7ad6d14fce9d0cd3ee6f
cntanos <cntanos@epu.ntua.gr>
2011-12-08 18:51:14 +0200

    rpc context functions updated

--- webinos/api/context/lib/webinos.rpc.context.js
+++ webinos/api/context/lib/webinos.rpc.context.js
@@ -1,22 +1,53 @@
-if (typeof webinos === 'undefined') var webinos = {};
-webinos.rpc = require('../../../common/rpc/lib/rpc.js');
-(function () {
+  if (typeof webinos === 'undefined') var webinos = {};
+
+  webinos.rpc = require('../../../common/rpc/lib/rpc.js');
 
   var RemoteContextManager = new RPCWebinosService({
     api:'http://webinos.org/api/context',
     displayName:'Context',
     description:'The webinos context manager'
   });
-  
+
+  var moduleRoot = require('../dependencies.json');
+  var dependencies = require('../' + moduleRoot.root.location + '/dependencies.json');
+  var webinosRoot = '../' + moduleRoot.root.location;
+
+  if (typeof webinos.context === 'undefined'){
+    console.log("context was not found!!!");
+    webinos.context = {};
+  }
+  debugger;
+  if (typeof webinos.context.DB === 'undefined'){
+    console.log("context.DB was not found!!!");
+    webinos.context.DB = {};
+    webinos.context.DB = require(webinosRoot + dependencies.manager.context_manager.location + 'lib/contextDBpzhManager.js');
+  }
   
   RemoteContextManager.find = function ( params,  successCallback,  errorCallback) {
     webinos.context.find(params[0],function(results){
       successCallback(results);
     },function(){
-      
+
     });
   };
 
-  webinos.rpc.registerObject(RemoteContextManager);
+  RemoteContextManager.executeQuery = function(query, successCallback, errorCallback){
+    switch(query.type)
+    {
+      case "DB-insert":
+        webinos.context.DB.insert(query.data); //TODO: Add success callback
+        break;
+      case "getrawview":
+        webinos.context.DB.getrawview(function(results){
+          successCallback(results);
+        });
+        break;
+      default:
+        errorCallback(new ContextError("Context Query Type '" + query.type + "' not found"))
+    }
 
-})();
+    function VehicleError(message){
+      this.message = message;
+    }
+  }
+  webinos.rpc.registerObject(RemoteContextManager);
