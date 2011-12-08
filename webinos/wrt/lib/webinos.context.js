9a48cede0f5e0ace969683dda4cd06c1fafffe84
cntanos <cntanos@epu.ntua.gr>
2011-12-08 15:33:50 +0200

    Context wrt

---
+++ webinos/wrt/lib/webinos.context.js
@@ -0,0 +1,55 @@
+(function() {
+
+	WebinosContext = function(obj) {
+		this.base = WebinosService;
+		this.base(obj);
+	};
+	
+	WebinosContext.prototype = new WebinosService;
+	
+	WebinosContext.prototype.bindService = function (bindCB, serviceId) {
+		// actually there should be an auth check here or whatever, but we just always bind
+		this.find = find;
+		this.executeQuery = executeQuery;
+		
+		if (typeof bindCB.onBind === 'function') {
+			bindCB.onBind(this);
+		};
+	}
+	
+	function find(params, successCB,errorCB) {
+		var rpc = webinos.rpc.createRPC(this, "find",  params);
+		webinos.rpc.executeRPC(rpc,
+				function (params){
+					successCB(params);
+				},
+				function (error){
+					errorCB(error);
+				}
+		);
+	}
+	 function executeQuery(query, successCB,errorCB) {
+	    var rpc = webinos.rpc.createRPC(this, "find",  query);
+	    webinos.rpc.executeRPC(rpc,
+	        function (params){
+	          successCB(params);
+	        },
+	        function (error){
+	          errorCB(error);
+	        }
+	    );
+	  }
+	
+	function echo(attr, successCB, errorCB) {
+		var rpc = webinos.rpc.createRPC(this, "echoAttr.echo", [attr]);
+		webinos.rpc.executeRPC(rpc,
+				function (params){
+					successCB(params);
+				},
+				function (error){
+					errorCB(error);
+				}
+		);
+	}
+	
+}());
