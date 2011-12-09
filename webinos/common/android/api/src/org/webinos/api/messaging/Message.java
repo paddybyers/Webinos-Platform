9c142bbad2de000b708fa310b0d38b93f4fd4083
Paddy Byers <paddy.byers@gmail.com>
2011-12-09 09:34:19 +0000

    Updated stubs to use Array types where appropriate

--- webinos/common/android/api/src/org/webinos/api/messaging/Message.java
+++ webinos/common/android/api/src/org/webinos/api/messaging/Message.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.meshpoint.anode.bridge.Env;
 import org.meshpoint.anode.idl.IDLInterface;
 import org.meshpoint.anode.java.Base;
+import org.w3c.dom.ObjectArray;
 import org.webinos.api.DeviceAPIError;
 import org.webinos.api.ErrorCallback;
 import org.webinos.api.File;
@@ -19,14 +20,14 @@ public abstract class Message extends Base {
 	public int folder;
 	public Date timestamp;
 	public String from;
-	public String[] to;
-	public String[] cc;
-	public String[] bcc;
+	public ObjectArray<String> to;
+	public ObjectArray<String> cc;
+	public ObjectArray<String> bcc;
 	public String body;
 	public boolean isRead;
 	public boolean priority;
 	public String subject;
-	public File[] attachments;
+	public ObjectArray<File> attachments;
 	
     public abstract PendingOperation update(UpdateMessageSuccessCallback successCallback, ErrorCallback errorCallback) throws DeviceAPIError;
 }
