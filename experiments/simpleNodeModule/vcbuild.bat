799b3a008652d6a296b1907d6ef902c06b6b2043
Andreas Botsikas <abot@epu.ntua.gr>
2011-12-13 20:27:29 +0200

    Fixed the vcbuild.bat in the simpleNodeModule to compile using only NODE_ROOT env variable

--- experiments/simpleNodeModule/vcbuild.bat
+++ experiments/simpleNodeModule/vcbuild.bat
@@ -1,13 +1,13 @@
 @echo OFF
 @rem Check for nodejs build location variable
-if not defined node_path goto nodebuild-not-found
-if not exist "%node_path%\src\node.h" goto nodebuild-not-found
-if not exist "%node_path%\deps\v8\include\v8.h" goto nodebuild-not-found
-if not exist "%node_path%\deps\uv\include\uv.h" goto nodebuild-not-found
+if not defined NODE_ROOT goto nodebuild-not-found
+if not exist "%NODE_ROOT%\src\node.h" goto nodebuild-not-found
+if not exist "%NODE_ROOT%\deps\v8\include\v8.h" goto nodebuild-not-found
+if not exist "%NODE_ROOT%\deps\uv\include\uv.h" goto nodebuild-not-found
 @rem detect the location of the node.lib file
 set nodelibpath=
-if exist "%node_path%\Release\node.lib" set nodelibpath=%node_path%\Release
-if not defined nodelibpath if exist "%node_path%\Debug\node.lib" set nodelibpath=%node_path%\Debug
+if exist "%NODE_ROOT%\Release\node.lib" set nodelibpath=%NODE_ROOT%\Release
+if not defined nodelibpath if exist "%NODE_ROOT%\Debug\node.lib" set nodelibpath=%NODE_ROOT%\Debug
 if not defined nodelibpath goto nodebuild-not-found
 @rem Check for visual studio tools if not already loaded
 if defined VCINSTALLDIR goto start-compilation
@@ -18,7 +18,7 @@ if not defined VCINSTALLDIR goto msbuild-not-found
 :start-compilation
 echo Compiling...
 @rem filename "don't strip comments" "no banner" "disable intrinsic functions" "no optimization" "calling conversion __cdecl" "no analysis" "the /I adds some folders in the include path" "no clr. This is deprecated and should be changed but I am working on a gyp file instead of this bat"
-cl.exe helloworld.cpp /c /nologo /Oi- /Od /Gd /analyze- /I%node_path%\src\ /I%node_path%\deps\v8\include\ /I%node_path%\deps\uv\include\ /clr:noAssembly
+cl.exe helloworld.cpp /c /nologo /Oi- /Od /Gd /analyze- /I%NODE_ROOT%\src\ /I%NODE_ROOT%\deps\v8\include\ /I%NODE_ROOT%\deps\uv\include\ /clr:noAssembly
 if errorlevel 1 goto exit-error
 echo Done compiling. Linking...
 echo Using %nodelibpath%\node.lib file to link to
@@ -26,8 +26,8 @@ link helloworld.obj node.lib /OUT:"helloworld.dll" /NOLOGO /DLL /NOENTRY /MANIFE
 if errorlevel 1 goto exit-error
 echo Done linking
 echo Cleaning up
-if not exist build\default mkdir build\default
-move helloworld.dll build\default\helloworld.node
+if not exist build mkdir build
+move helloworld.dll build\helloworld.node
 del helloworld.obj helloworld.lib helloworld.exp
 echo Finished
 goto exit
@@ -36,7 +36,7 @@ echo Visual studio tools were not found! Please check the VS100COMNTOOLS path va
 goto exit
 
 :nodebuild-not-found
-echo Node build path not found! Please check the node_path path variable exists and that it points to the root of the git repo where you have build 
+echo Node build path not found! Please check the NODE_ROOT path variable exists and that it points to the root of the git repo where you have build 
 goto exit
 :exit-error
 echo An error occured. Please check the above output
