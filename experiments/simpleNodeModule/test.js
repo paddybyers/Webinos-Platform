f23a283f8cbb00701db4377096f30a5d7d1b28eb
Andreas Botsikas <abot@epu.ntua.gr>
2011-12-15 18:33:01 +0200

    Fixed the helloworld test file to reflect new node location

--- experiments/simpleNodeModule/test.js
+++ experiments/simpleNodeModule/test.js
@@ -1,6 +1,6 @@
 console.log("Loading module from :");
-console.log("> " + __dirname + "/build/helloworld.node");
-var helloWorld = require(__dirname + "/build/helloworld.node");
+console.log("> " + __dirname + "/build/Release/helloworld.node");
+var helloWorld = require(__dirname + "/build/Release/helloworld.node");
 tester = new helloWorld.HelloWorld();
 console.log("Testing module:");
 console.log("> " + tester.hello());
