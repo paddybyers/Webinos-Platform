bd96a8cd351bb51af7cc0f885465939f4085d0b0
Andreas Botsikas <abot@epu.ntua.gr>
2011-12-15 10:26:32 +0200

    Updated webinos-gyp to access make in windows to build the solution without having to open visual studio. Just use webinos-gyp make or webinos-gyp gypfilename make. In linux you can do webinos-gyp && make

--- Tools/gyp_tools/webinos-gyp.bat
+++ Tools/gyp_tools/webinos-gyp.bat
@@ -11,17 +11,35 @@ set node_lib_folder=
 if exist "%NODE_ROOT%\Release\node.lib" set node_lib_folder=Release
 if not defined node_lib_folder if exist "%NODE_ROOT%\Debug\node.lib" set node_lib_folder=Debug
 if not defined node_lib_folder goto nodebuild-not-found
-
+@rem Check if user has specified the build command
+set requestedBuild= 0
+@rem Check if the first argument is make
+if "%1" == "make" set requestedBuild= 1
+@rem Check if the second argument is make (assuming the first one is the gyp file)
+if "%2" == "make" set requestedBuild= 1
 @rem Try to locate the gyp file
 set gypfile=
-@rem if the user has specified the file, this is the one we will use
-if exist %1 set gypfile=%1
+@rem if the user has specified the file, this is the one we will use. The file must have a gyp extension
+if defined %1 if exist %1 if "%~x1" == ".gyp" set gypfile=%1
 @rem otherwise try to locate the module.gyp file
 if not defined gypfile if exist "%CD%\module.gyp" set gypfile=module.gyp
 if not defined gypfile goto gyp-file-missing
 @rem Generate visual studio solution
 python %NODE_ROOT%\tools\gyp\gyp -f msvs -G msvs_version=2010 %gypfile% --depth=. -DNODE_ROOT=%NODE_ROOT% -Dnode_lib_folder=%node_lib_folder%
 if errorlevel 1 goto exit-error
+if %requestedBuild% == 0 goto Finished
+@rem Build the solution
+@rem Check for visual studio tools if not already loaded
+if defined VCINSTALLDIR goto BuildSolution
+@rem Ensure that visual studio is available
+if not defined VS100COMNTOOLS goto msbuild-not-found
+if not exist "%VS100COMNTOOLS%..\..\vc\vcvarsall.bat" goto msbuild-not-found
+call "%VS100COMNTOOLS%..\..\vc\vcvarsall.bat"
+@rem Check that vs is properly loaded
+if not defined VCINSTALLDIR goto msbuild-not-found
+:BuildSolution
+call :BuildSolutionBasedOnGypFilename %gypfile%
+:Finished
 echo Finished
 goto exit
 :msbuild-not-found
@@ -36,9 +54,25 @@ goto exit
 :gyp-file-missing
 echo Could not locate a gyp file. No module.gyp file found and you haven't specified any existing gyp file as an argument
 goto exit
+:msbuild-not-found
+echo Visual studio tools were not found! Please check the VS100COMNTOOLS path variable
+goto exit
+:solutions-file-not-found
+echo Generated solution file %generatedslnfile% was not found!
+goto exit
 :exit-error
 echo An error occured. Please check the above output
 :exit
 @rem clear local variables
 set node_lib_folder=
+set requestedBuild=
 set gypfile=
+goto :EOF
+@rem Internal procedures
+:BuildSolutionBasedOnGypFilename
+set generatedslnfile=%~n1.sln
+if not exist %generatedslnfile% goto solutions-file-not-found
+@rem call msbuild to build the sln file
+msbuild.exe %generatedslnfile%
+if errorlevel 1 goto exit-error
+goto :EOF
