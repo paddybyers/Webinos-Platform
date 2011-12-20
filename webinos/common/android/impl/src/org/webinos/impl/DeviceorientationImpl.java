84209b0a6e2cd438ed49f3ad05c740730d6cfcd8
Paddy Byers <paddy.byers@gmail.com>
2011-12-20 16:58:06 +0000

    Updated deviceorientation implementation

--- webinos/common/android/impl/src/org/webinos/impl/DeviceorientationImpl.java
+++ webinos/common/android/impl/src/org/webinos/impl/DeviceorientationImpl.java
@@ -8,9 +8,9 @@ import org.webinos.api.deviceorientation.MotionCB;
 import org.webinos.api.deviceorientation.MotionEvent;
 import org.webinos.api.deviceorientation.OrientationCB;
 import org.webinos.api.deviceorientation.OrientationEvent;
+import org.webinos.api.deviceorientation.RotationRate;
 
 import org.meshpoint.anode.AndroidContext;
-import org.meshpoint.anode.bridge.Env;
 import org.meshpoint.anode.module.IModule;
 import org.meshpoint.anode.module.IModuleContext;
 
@@ -19,6 +19,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
 public class DeviceorientationImpl extends DeviceorientationManager implements
 		IModule {
@@ -26,14 +27,14 @@ public class DeviceorientationImpl extends DeviceorientationManager implements
 	private Context androidContext;
 	private SensorManager sensorManager;
 
-	private OrientationCB orientationCb;
 	private OrientationListener orientationListener;
 	private Sensor orientationSensor;
+	private Sensor magneticSensor;
 
-	private MotionCB motionCb;
 	private AccelerometerListener accelerometerListener;
 	private Sensor accelerometerSensor;
-	private long lastEventTime;
+	private Sensor linearAccelerometerSensor;
+	private boolean integrateLinearAcceleration;
 
 	private static final String TAG = "org.webinos.impl.DeviceorientationImpl";
 	
@@ -42,23 +43,25 @@ public class DeviceorientationImpl extends DeviceorientationManager implements
 	 *****************************/
 	@Override
 	public synchronized void watchOrientation(OrientationCB orientationCb) {
-		this.orientationCb = orientationCb;
-		orientationListener = new OrientationListener();
+		(orientationListener = new OrientationListener(orientationCb)).start();
 		sensorManager.registerListener(orientationListener, orientationSensor, SensorManager.SENSOR_DELAY_FASTEST);
 	}
 
 	@Override
 	public synchronized void watchMotion(MotionCB motionCb) {
-		this.motionCb = motionCb;
-		accelerometerListener = new AccelerometerListener();
+		(accelerometerListener = new AccelerometerListener(motionCb)).start();
 		sensorManager.registerListener(accelerometerListener, accelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
-		lastEventTime = System.currentTimeMillis();
+		if(linearAccelerometerSensor != null)
+			sensorManager.registerListener(accelerometerListener, linearAccelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
+		if(magneticSensor != null)
+			sensorManager.registerListener(accelerometerListener, magneticSensor, SensorManager.SENSOR_DELAY_FASTEST);
 	}
 
 	@Override
 	public synchronized void unwatchOrientation() {
 		if(orientationListener != null) {
 			sensorManager.unregisterListener(orientationListener);
+			orientationListener.kill();
 			orientationListener = null;
 		}
 	}
@@ -67,6 +70,7 @@ public class DeviceorientationImpl extends DeviceorientationManager implements
 	public synchronized void unwatchMotion() {
 		if(accelerometerListener != null) {
 			sensorManager.unregisterListener(accelerometerListener);
+			accelerometerListener.kill();
 			accelerometerListener = null;
 		}
 	}
@@ -78,19 +82,38 @@ public class DeviceorientationImpl extends DeviceorientationManager implements
 	public Object startModule(IModuleContext ctx) {
 		androidContext = ((AndroidContext)ctx).getAndroidContext();
 		sensorManager = (SensorManager)androidContext.getSystemService(Context.SENSOR_SERVICE);
+		
+		/* get orientation sensor - legacy devices */
 		List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ORIENTATION);
 		if(sensorList.isEmpty())
-			Env.logger.e(TAG, "No orientation device found");
+			Log.e(TAG, "No orientation device found");
 		else
 			orientationSensor = sensorList.get(0);
-		sensorList = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+
+		/* get magnetic field sensor */
+		sensorList = sensorManager.getSensorList(Sensor.TYPE_MAGNETIC_FIELD);
 		if(sensorList.isEmpty())
-			Env.logger.e(TAG, "No accelerometer device found");
+			Log.e(TAG, "No magnetic field device found");
 		else
+			magneticSensor = sensorList.get(0);
+
+		/* get motion sensors */
+		sensorList = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+		if(sensorList.isEmpty()) {
+			Log.e(TAG, "No accelerometer device found");
+		} else {
 			accelerometerSensor = sensorList.get(0);
+			sensorList = sensorManager.getSensorList(Sensor.TYPE_LINEAR_ACCELERATION);
+			if(sensorList.isEmpty()) {
+				Log.e(TAG, "No linear accelerometer device found");
+				integrateLinearAcceleration = true;
+			} else {
+				linearAccelerometerSensor = sensorList.get(0);
+			}
+		}
 		
 		if(accelerometerSensor == null && orientationSensor == null) {
-			Env.logger.e(TAG, "No orientation or accelerometer device found - aborting");
+			Log.e(TAG, "No orientation or accelerometer device found - aborting");
 			return null;
 		}
 		return this;
@@ -106,7 +129,42 @@ public class DeviceorientationImpl extends DeviceorientationManager implements
 	 * Helpers
 	 *****************************/
 	
-	class OrientationListener implements SensorEventListener {
+	class OrientationListener extends Thread implements SensorEventListener {
+
+		private OrientationCB orientationCb;
+		private OrientationEvent pendingEvent;
+		private boolean isKilled;
+
+		private OrientationListener(OrientationCB orientationCb) {
+			this.orientationCb = orientationCb;
+		}
+
+		private void kill() {
+			isKilled = true;
+			interrupt();
+		}
+		
+		private synchronized void postOrientation(OrientationEvent ev) {
+			pendingEvent = ev;
+			notify();
+		}
+		
+		@Override
+		public void run() {
+			while(!isKilled) {
+				synchronized(this) {
+					try {
+						wait();
+					} catch(InterruptedException ie) { break; }
+					if(pendingEvent != null) {
+						OrientationEvent ev = pendingEvent;
+						pendingEvent = null;
+						if(orientationCb != null)
+							orientationCb.onOrientationEvent(ev);
+					}
+				}
+			}
+		}
 
 		@Override
 		public void onAccuracyChanged(Sensor arg0, int arg1) {}
@@ -118,36 +176,147 @@ public class DeviceorientationImpl extends DeviceorientationManager implements
 			ev.beta = sensorEvent.values[1];
 			ev.gamma = sensorEvent.values[2];
 			ev.absolute = true;
-			synchronized(DeviceorientationImpl.this) {
-				if(orientationCb != null)
-					orientationCb.onOrientationEvent(ev);
-			}
+			postOrientation(ev);
 		}
 	}
 	
-	class AccelerometerListener implements SensorEventListener {
+	class AccelerometerListener extends Thread implements SensorEventListener {
+		
+		private MotionCB motionCb;
+
+		private float[] lastAccelerationValues;
+		private float[] lastMagneticValues;
+		private long lastMagneticTime;
+		private float[] R = new float[9];
+		private float[] I = new float[9];
+		private float[] orientation = new float[3];
+
+		private Acceleration pendingAcceleration;
+		private Acceleration pendingLinearAcceleration;
+		private Acceleration gravity;
+		private MotionEvent pendingEvent;
+		private long lastMotionEventTime;
+		
+		private OrientationEvent lastOrientation;
+		private RotationRate lastRotationRate;
+		private long lastOrientationEventTime;
+
+		private boolean isKilled;
+		private final float alpha = 0.8F;
+		private static final double degreesPerRadian = 180 / Math.PI;
+		
+		private AccelerometerListener(MotionCB motionCb) {
+			this.motionCb = motionCb;
+			lastMotionEventTime = System.currentTimeMillis();
+			if(integrateLinearAcceleration)
+				gravity = new Acceleration();
+		}
+		
+		private void kill() {
+			isKilled = true;
+			interrupt();
+		}
+		
+		private synchronized void updateRotationRate(OrientationEvent ev, long time) {
+			long timeDelta = time - lastOrientationEventTime;
+			if(timeDelta > 0 && lastOrientationEventTime > 0) {
+				RotationRate result = new RotationRate();
+				double factor = degreesPerRadian * 1000 / timeDelta;
+				result.alpha = factor * (ev.alpha - lastOrientation.alpha);
+				result.beta = factor * (ev.beta - lastOrientation.beta);
+				result.gamma = factor * (ev.alpha - lastOrientation.alpha);
+				lastOrientation = ev;
+				lastOrientationEventTime = time;
+				lastRotationRate = result;
+			}
+		}
+
+		private synchronized void postAcceleration(Acceleration acc, Acceleration lin) {
+			MotionEvent ev = new MotionEvent();
+			ev.acceleration = lin;
+			ev.accelerationIncludingGravity = acc;
+			ev.rotationRate = lastRotationRate;
+
+			long thisEventTime = System.currentTimeMillis();
+			ev.interval = (double)(thisEventTime - lastMotionEventTime);
+			lastMotionEventTime = thisEventTime;
+			pendingEvent = ev;
+			notify();
+		}
+		
+		@Override
+		public void run() {
+			while(!isKilled) {
+				synchronized(this) {
+					try {
+						wait();
+					} catch(InterruptedException ie) { break; }
+					if(pendingEvent != null) {
+						MotionEvent ev = pendingEvent;
+						pendingEvent = null;
+						if(motionCb != null)
+							motionCb.onMotionEvent(ev);
+					}
+				}
+			}
+		}
 
 		@Override
 		public void onAccuracyChanged(Sensor arg0, int arg1) {}
 
 		@Override
 		public void onSensorChanged(SensorEvent sensorEvent) {
-			MotionEvent ev = new MotionEvent();
-			Acceleration acc = new Acceleration();
-			Acceleration g_acc = new Acceleration();
-			acc.x = g_acc.x = sensorEvent.values[SensorManager.AXIS_X - 1];
-			acc.y = g_acc.y = sensorEvent.values[SensorManager.AXIS_Y - 1];
-			acc.z = sensorEvent.values[SensorManager.AXIS_Z - 1];
-			g_acc.z = acc.z + 1;
-			ev.acceleration = acc;
-			ev.accelerationIncludingGravity = g_acc;
-			/* FIXME: work out how to implement rotation */
-			synchronized(DeviceorientationImpl.this) {
-				long thisEventTime = System.currentTimeMillis();
-				ev.interval = (double)(thisEventTime - lastEventTime);
-				if(motionCb != null)
-					motionCb.onMotionEvent(ev);
-				lastEventTime = thisEventTime;
+			Acceleration acc = null, lin = null;		
+			if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+				lastAccelerationValues = sensorEvent.values.clone();
+				acc = new Acceleration();
+				acc.x = sensorEvent.values[SensorManager.AXIS_X - 1];
+				acc.y = sensorEvent.values[SensorManager.AXIS_Y - 1];
+				acc.z = sensorEvent.values[SensorManager.AXIS_Z - 1];
+				if(integrateLinearAcceleration) {
+					gravity.x = alpha * gravity.x + (1 - alpha) * acc.x;
+					gravity.y = alpha * gravity.y + (1 - alpha) * acc.y;
+					gravity.z = alpha * gravity.z + (1 - alpha) * acc.z;
+
+					lin = new Acceleration();
+					lin.x = acc.x - gravity.x;
+					lin.y = acc.y - gravity.y;
+					lin.z = acc.z - gravity.z;
+					
+					postAcceleration(acc, lin);
+					return;
+				}
+				pendingAcceleration = acc;
+			} else if(sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+				lin = new Acceleration();
+				lin.x = sensorEvent.values[SensorManager.AXIS_X - 1];
+				lin.y = sensorEvent.values[SensorManager.AXIS_Y - 1];
+				lin.z = sensorEvent.values[SensorManager.AXIS_Z - 1];
+				pendingLinearAcceleration = lin;
+			} else if(sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+				lastMagneticValues = sensorEvent.values.clone();
+				lastMagneticTime = sensorEvent.timestamp;
+			}
+			/*
+			 * This procedure to combine information from the magnetic field sensor and the
+			 * accelerometer to get the rotation matrix, and hence the orientation.
+			 * However, it doesn't seem to give meaningful data on the Nexus S at least.
+			 * So to get orientation, we're sticking with the (legacy) ORIENTATION sensor
+			 * for now.
+			 */
+			if(lastAccelerationValues != null && lastMagneticValues != null) {
+				SensorManager.getRotationMatrix(R, I, lastAccelerationValues, lastMagneticValues);
+				SensorManager.getOrientation(R, orientation);
+				OrientationEvent orientationEvent = new OrientationEvent();
+				orientationEvent.alpha = orientation[0];
+				orientationEvent.beta = orientation[1];
+				orientationEvent.gamma = orientation[2];
+				updateRotationRate(orientationEvent, lastMagneticTime);
+				lastAccelerationValues = lastMagneticValues = null;
+			}
+			if(pendingAcceleration != null && pendingLinearAcceleration != null) {
+				postAcceleration(pendingAcceleration, pendingLinearAcceleration);
+				pendingAcceleration = pendingLinearAcceleration = null;
 			}
 		}		
 	}
