fb3fa1eec6f81f97eff3f63cc9874a4469e98898
habib.virji <habib.virji@samsung.com>
2011-12-22 16:28:54 +0000

    Startup script changes to include webServerPort and serverPort in startPzh.js StartPzp.js modified to have webServerPort first and then serverPort
    Changes in session common to support first lookup, if it fails then it uses DNS resolve. This is to make it work for localhost scenario.
    
    Tested only on Linux

--- demo/startPzp.js
+++ demo/startPzp.js
@@ -1,15 +1,15 @@
 pzp = require('../webinos/pzp/lib/session_pzp.js');
 
-var ipAddr = '127.0.0.1' , port = 8000, serverPort = 8081, webServerPort = 8080;
+var ipAddr = 'localhost' , port = 8000, webServerPort = 8080, serverPort = 8081;
 process.argv.forEach(function(val, index, array) {
 	if(index === 2) 
 		ipAddr = val;
 	else if (index === 3)
 		port = val;
 	else if (index === 4)
-		serverPort = val;
-	else if (index === 5)
 		webServerPort = val;
+	else if (index === 5)
+		serverPort = val;	
 });
 
 var pzpModules = {};
@@ -29,7 +29,7 @@ pzpModules.list = [
 ];
 
 if (ipAddr === '' || port <= 0) {
-	console.log("Error starting Pzp.\n\t Start with: node startPzp.js <host> <port> \n\t E.g.: node startPzp.js localhost 8000 8081 8080");
+	console.log("Error starting Pzp.\n\t Start with: node startPzp.js <host> <port> <webServerPort> <serverPort> \n\t E.g.: node startPzp.js localhost 8000 8080 8081");
 } else {
 	var contents ="pzh_name=localhost\ncountry=UK\nstate=MX\ncity=ST\norganization=Webinos\norganizationUnit=WP4\ncommon=WebinosPzp\nemail=internal@webinos.org\ndays=180\n"
 	pzp.startPzpWebSocketServer(ipAddr, serverPort, webServerPort, pzpModules);
