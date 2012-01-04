de5e406048a1c87ebb1458d4f5839dcff15b9145
Paddy Byers <paddy.byers@gmail.com>
2012-01-04 22:48:07 +0000

    Do not implement IModule

--- webinos/common/android/impl/src/org/webinos/impl/MessageImpl.java
+++ webinos/common/android/impl/src/org/webinos/impl/MessageImpl.java
@@ -1,44 +1,19 @@
 package org.webinos.impl;
 
-import org.meshpoint.anode.AndroidContext;
-import org.meshpoint.anode.module.IModule;
-import org.meshpoint.anode.module.IModuleContext;
 import org.webinos.api.DeviceAPIError;
 import org.webinos.api.ErrorCallback;
 import org.webinos.api.PendingOperation;
 import org.webinos.api.messaging.Message;
 import org.webinos.api.messaging.UpdateMessageSuccessCallback;
 
-import android.content.Context;
 import android.util.Log;
 
-public class MessageImpl extends Message implements IModule {
+public class MessageImpl extends Message {
 
-	private Context androidContext;
 	private static final String LABEL = "WebinosMessaging";
 	
 	@Override
     public PendingOperation update(UpdateMessageSuccessCallback successCallback, ErrorCallback errorCallback) throws DeviceAPIError {
 		return null;
-	}
-
-    /*****************************
-	 * IModule methods
-	 *****************************/
-	@Override
-	public Object startModule(IModuleContext ctx) {
-		Log.d(LABEL, "MessageImpl - startModule");
-		androidContext = ((AndroidContext)ctx).getAndroidContext();
-		/*
-		 * perform any module initialisation here ...
-		 */
-		return this;
-	}
-
-	@Override
-	public void stopModule() {
-		/*
-		 * perform any module shutdown here ...
-		 */
 	}
 }
