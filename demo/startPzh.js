30e26a226b305bf4e36bc155c17166feb25f6644
Alexander Futasz <alexander.futasz@fokus.fraunhofer.de>
2012-01-26 14:40:26 +0100

    Introduce arguments parsing for startPzh.js/startPzp.js
    You can now specify ports and other options on the commandline, use --help
    for details. Or specify the options in a config-pzp.json, -pzh respectively
    , file in the same directory.

--- demo/startPzh.js
+++ demo/startPzh.js
@@ -1,33 +1,98 @@
-Pzh            = require('../webinos/pzh/lib/pzh_sessionHandling.js');
-WebSocket      = require('../webinos/pzh/lib/pzh_websocket.js');
+var Pzh            = require('../webinos/pzh/lib/pzh_sessionHandling.js');
+	WebSocket      = require('../webinos/pzh/lib/pzh_websocket.js'),
+	fs = require('fs'),
+	path = require('path');
 
-var ipAddr = 'localhost', port = 8000, serverPort = 8083, webServerPort = 8082;
+var options = {};
 
-process.argv.forEach(function(val, index, array) {
-	if(index === 2) 
-		ipAddr = val;
-	else if (index === 3)
-		port = val;
-	else if (index === 4)
-		webServerPort = val;
-	else if (index === 5)
-		serverPort = val;
+function help() {
+    console.log('Usage: node startPzh.js [options]');
+    console.log('Options:');
+    console.log('--host=[host]            host of the pzh (default localhost)');
+    console.log('--port=[port]            port to host the pzh (default 8000)');
+    console.log('--pzh-http-port=[port]   port to pzp web server (default 8080)');
+    console.log('--pzh-ws-port=[port]     port to pzp websocket server (default 8081)');
+    process.exit();
+}
+
+process.argv.forEach(function (arg) {
+	  var parts;
+	  if (arg.indexOf('--') > -1) {
+	    parts = arg.split('=');
+	    if (parts.length > 1) {
+	      switch (parts[0]) {
+	      case '--host':
+	        options.host = parts[1];
+	        break;
+	      case '--port':
+	    	  options.port = parseInt(parts[1], 10);
+	    	  break;
+	      case '--pzh-ws-port':
+	    	  options.pzhHttpPort = parseInt(parts[1], 10);
+	    	  break;
+	      case '--pzh-http-port':
+	    	  options.pzhWebsocketPort = parseInt(parts[1], 10);
+	    	  break;
+	      default:
+	        console.log('unknown option: ' + parts[0]);
+	        break;
+	      }
+	    }
+	    else if (parts[0] === '--help') {
+	    	help();
+	    }
+	  }
 });
 
 var pzhModules = [
     {name: "get42", params: [99]}
 ];
 
-if (ipAddr === '' || port <= 0) {
-	console.log("Error starting server.\n\t Start with: node startPzh.js <host> <port> <webserverPort> <serverPort>) \n\t E.g.: node startPzh.js localhost 8000 8082 8083");
+if (options.host === '' || options.port <= 0) {
+	help();
 } else {
-	var contents ="country=UK\nstate=MX\ncity=ST\norganization=Webinos\norganizationUnit=WP4\ncommon=WebinosPzh\nemail=internal@webinos.org\ndays=180\n" ;
-	WebSocket.startServer(ipAddr, serverPort, webServerPort, function() {
-		Pzh.startPzh(contents, ipAddr, port, pzhModules, function() {
+	fs.readFile(path.join(__dirname, 'config-pzh.json'), function(err, data) {
+		var config;
+		
+		if (err) {
+			console.warn("could not load config-pzh.json\n" + err.toString());
+			config = {};
+		}
+		else {
+			config = JSON.parse(data);
+		}
+		
+		if (!config.host) {
+			config.host = 'localhost';
+		}
+		if (!config.port) {
+			config.port = 8000;
+		}
+		if (!config.pzhHttpPort) {
+			config.pzhHttpPort = 8082;
+		}
+		if (!config.pzhWebsocketPort) {
+			config.pzhWebsocketPort = 8083;
+		}
+		if (options.pzhHost) {
+			config.host = options.host;
+		}
+		if (options.port) {
+			config.port = options.port;
+		}
+		if (options.pzhHttpPort) {
+			config.pzhHttpPort = options.pzhHttpPort;
+		}
+		if (options.pzhWebsocketPort) {
+			config.pzhWebsocketPort = options.pzhWebsocketPort;
+		}
 
-			console.log('=== PZH STARTED ===');
+		var contents ="country=UK\nstate=MX\ncity=ST\norganization=Webinos\norganizationUnit=WP4\ncommon=WebinosPzh\nemail=internal@webinos.org\ndays=180\n" ;
+		WebSocket.startServer(config.host, config.pzhHttpPort, config.pzhWebsocketPort, function() {
+			Pzh.startPzh(contents, config.host, config.port, pzhModules, function() {
+				console.log('=== PZH STARTED ===');
+			});
 		});
 	});
-	
 }
 
