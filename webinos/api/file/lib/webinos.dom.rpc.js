d7d1bcfadd21d84bafa58769369eaeb92efb6d6a
Felix-Johannes Jendrusch <felix-johannes.jendrusch@fokus.fraunhofer.de>
2012-01-30 14:07:51 +0100

    file-api: More server-side refactoring.

--- webinos/api/file/lib/webinos.dom.rpc.js
+++ webinos/api/file/lib/webinos.dom.rpc.js
@@ -1,52 +1,48 @@
 (function (exports) {
 	"use strict";
 
-	exports.DOMException = {
-		serialize: function (exception) {
-			return {
-				name: exception.name,
-				message: exception.message,
-				code: exception.code
-			};
-		}
+	exports.DOMException = {};
+	exports.DOMException.serialize = function (exception) {
+		return {
+			name: exception.name,
+			message: exception.message,
+			code: exception.code
+		};
 	};
 
-	exports.DOMError = {
-		serialize: function (error) {
-			return {
-				name: error.name
-			};
-		}
+	exports.DOMError = {};
+	exports.DOMError.serialize = function (error) {
+		return {
+			name: error.name
+		};
 	};
 
-	exports.Event = {
-		serialize: function (event) {
-			return {
-				type: event.type,
-				// eventPhase: event.eventPhase,
-				// stopPropagation: event.stopPropagation,
-				// stopImmediatePropagation: event.stopImmediatePropagation,
-				// canceled: event.canceled,
-				// initialized: event.initialized,
-				// dispatch: event.dispatch,
-				bubbles: event.bubbles,
-				cancelable: event.cancelable,
-				// defaultPrevented: event.defaultPrevented,
-				// isTrusted: event.isTrusted,
-				// timeStamp: event.timeStamp
-			};
-		}
+	exports.Event = {};
+	exports.Event.serialize = function (event) {
+		return {
+			type: event.type,
+			// eventPhase: event.eventPhase,
+			// stopPropagation: event.stopPropagation,
+			// stopImmediatePropagation: event.stopImmediatePropagation,
+			// canceled: event.canceled,
+			// initialized: event.initialized,
+			// dispatch: event.dispatch,
+			bubbles: event.bubbles,
+			cancelable: event.cancelable,
+			// defaultPrevented: event.defaultPrevented,
+			// isTrusted: event.isTrusted,
+			// timeStamp: event.timeStamp
+		};
 	};
 
-	exports.ProgressEvent = {
-		serialize: function (event) {
-			var object = exports.Event.serialize(event);
+	exports.ProgressEvent = {};
+	exports.ProgressEvent.serialize = function (event) {
+		var object = exports.Event.serialize(event);
 
-			object.lengthComputable = event.lengthComputable;
-			object.loaded = event.loaded;
-			object.total = event.total;
+		object.lengthComputable = event.lengthComputable;
+		object.loaded = event.loaded;
+		object.total = event.total;
 
-			return object;
-		}
+		return object;
 	};
 })(module.exports);
