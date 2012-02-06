ed191e23a7d8e9240098616e0b7b8ccbc9620f99
Paolo Vergori <Gregg@Greggs-MacBook.local>
2012-02-06 17:32:04 +0100

    contacts: android test

---
+++ webinos/api/contacts/test/android_contacts_test/androidTest.js
@@ -0,0 +1,34 @@
+var util = require('util');
+var contacts = require('bridge').load('org.webinos.impl.ContactManagerImpl', this);
+
+console.log("javaBridge loaded Exposed methods: ");
+for(var i in contacts)
+	console.log(i + ': ' + contacts[i]);
+
+var opt = new Array();
+
+var fields = {};
+
+fields["name"] = "Paolo";
+//fields["name"] = "Darth - Vader, Jr.";
+fields["phoneNumber"] = "+460123456789";
+fields["nickname"] = "Darthy";
+//fields["organization"] = "Empire ORG.";
+//fields["email"] = "darth@empire.net";
+fields["email"] = "vergori@ismb.it";
+//fields["address"] = "Empire";
+//fields["im"] = "darth.vader";
+//fields["url"] = "www.empire.org";
+
+
+function successCB(tmp)
+{	
+	for(var i=0;i<tmp.length;i++) {
+		console.log(util.inspect(tmp[i].name));
+	}
+}
+
+	
+contacts.find(fields, successCB, function(){}, opt);
+
+
