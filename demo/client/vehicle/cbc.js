062057fc825b37a667a8d8281200852bc78ac44f
Simon Isenberg <simon.isenberg@bmw.de>
2012-02-16 15:33:40 +0100

    continued MWC vehicle demo

--- demo/client/vehicle/cbc.js
+++ demo/client/vehicle/cbc.js
@@ -11,6 +11,34 @@ var vehicle;
 var geolocation;
 var ps;
 
+var gear;
+
+
+var active = '#drive';
+
+var selecterOn = false;
+var pdcEnabled = false;
+
+var currentCustomField = '';
+
+
+
+dataModel = [{id: 'selecter-gear', desc:'Gear' , unit: '', defaultV:'N', customField: 'customfield1'}, 
+{id: 'selecter-speed', desc:'Speed' , unit: 'kmph', defaultV:'00.0', customField: 'customfield2'}, 
+{id: 'selecter-heading', desc:'Heading' , unit: '&deg;', defaultV:'00.0', customField: null},
+{id: 'selecter-lateral', desc:'Lateral Acceleration' , unit: 'm/s<sup>2</sup>', defaultV:'00.0', customField: null}, 
+{id: 'selecter-longitudinal', desc:'Longitudinal Acceleration' , unit: 'm/s<sup>2</sup>', defaultV:'00.0', customField: null}, 
+{id: 'selecter-lat', desc:'Latitude' , unit: '&deg;', defaultV:'00.00.00', customField: 'customfield3'},
+{id: 'selecter-lng', desc:'Longitude' , unit: '&deg;', defaultV:'00.00.00', customField: 'customfield4'},
+{id: 'selecter-alt', desc:'Altitude' , unit: 'm', defaultV:'0', customField: 'customfield5'},
+{id: 'selecter-consumption', desc:'Average Consumption' , unit: 'l/100km', defaultV:'0.0', customField: null},
+{id: 'selecter-avg-speed', desc:'Average Speed' , unit: 'kmh', defaultV:'00.0', customField: null},
+{id: 'selecter-mileage', desc:'Mileage' , unit: 'km', defaultV:'0000.0', customField: null},
+{id: 'selecter-distance', desc:'Trip Distance' , unit: 'km', defaultV:'0.0', customField: null},
+{id: 'selecter-range', desc:'Range' , unit: 'km', defaultV:'0.0', customField: null},
+{id: 'selecter-time', desc:'Time' , unit: 'hh:mm', defaultV:'00:00', customField: null}];
+
+
 function initializeMap() {
     var mapOptions = {
       zoom: 13,
@@ -37,8 +65,25 @@ function updateStatus(text){
 	$('#loadingstatus').html(text);
 }
 
-	
 
+				function getLinkObj(hash){
+					switch(hash){
+						case '#drive':
+							return '#nav1';
+							break;
+						case '#travel':
+							return '#nav2';
+							break;
+						case '#check':
+							return '#nav3';
+							break;
+						case '#geek':
+							return '#nav4';
+							break;
+					}
+					
+					
+				}
 
 				function logMessage(msg) {
                                 	if (msg) {
@@ -48,15 +93,89 @@ function updateStatus(text){
 
 
 						$(document).ready(function() {
-								$(window).bind('hashchange', function() {
-  										switch(window.location.hash){
-  											case '#travel':
-  												break;
-  											default:
-  												alert(window.location.hash);
-  												break;
-  										}
+						
+								$('#nav1').focus();
+								//HELPERS FOR OVERSCROLLING SELECTION LIST
+								$('#selection-start').bind('focus', function(){
+									$('#selecter-time').focus();
 								});
+
+								$('#selection-end').bind('focus', function(){
+									$('#selecter-speed').focus();
+								});
+								
+								$('#n-start').bind('focus', function(){
+									$('#nav1').focus();
+								});
+								
+								$('#n-end').bind('focus', function(){
+									if(active != '#geek'){
+										$('#nav4').focus();
+									}
+								});
+								
+								$('#geek-end').bind('focus', function(){
+									$('#customfield5').focus();
+								});
+								$('a[id*="selecter-"]').bind('click', function(){
+									$('#selection').fadeOut(500);
+									console.log(this.id);
+									for(var i = 0; i < dataModel.length; i++){
+										if(dataModel[i].customField == currentCustomField){
+											dataModel[i].customField = null;
+										}
+										if(this.id == dataModel[i].id){
+											//SETTING NEW DATA ON FIELD;
+											$('#'+currentCustomField).find(".unit").html(dataModel[i].unit);
+											$('#'+currentCustomField).find(".value").html(dataModel[i].defaultV);
+											$('#'+currentCustomField).find(".description").html(dataModel[i].desc);
+											dataModel[i].customField = currentCustomField;
+											break;
+										}
+									}
+									$('#'+currentCustomField).focus();
+								});
+								
+								$('a[id*="customfield"]').bind('click', function(){
+									console.log(this.id);
+									currentCustomField = this.id;
+									selecterOn = true;
+									$('#selection').fadeIn(500);
+  									
+  									
+  									$('a[id*="selecter-"]').removeClass('selected');
+  									$('a[id*="selecter-"]').removeClass('hidden');
+  									
+  									for(var i = 0; i < dataModel.length; i++){
+  										
+  										if(dataModel[i].customField == this.id){
+  											$('#' + dataModel[i].id).addClass('selected');
+  											$('#' + dataModel[i].id).focus();
+  										}else if(dataModel[i].customField != null){
+  											$('#' + dataModel[i].id).addClass('hidden');
+  										}
+  									}
+  									
+								});
+								
+								$(window).bind('hashchange', function() {
+  										if((window.location.hash == '#drive' || window.location.hash == '#travel' || window.location.hash == '#check' || window.location.hash == '#geek') && active != window.location.hash){
+  											$(active).fadeOut(250);
+  											$(getLinkObj(active)).removeClass('active');
+  											active = window.location.hash;
+  											$(active).fadeIn(500);
+  											$(getLinkObj(active)).addClass('active');
+  										}
+  										
+  										if(window.location.hash == '#geek' && selecterOn){
+  											selecterOn == false;
+  											$('#selection').fadeOut(500);
+  											$('#'+currentCustomField).focus();
+  										
+  										}
+  										
+  										
+  								});
 						
 						
   								initializeMap();
@@ -156,7 +275,8 @@ function updateStatus(text){
                         {onFound: function (service) {
                             updateStatus('geolocation service found');
                             geolocation = service;
-            				bindToGeolocation();        		
+            				bindToGeolocation();
+            				$('#loading').addClass('disabled');
                     }});
 		}
 		
@@ -180,15 +300,29 @@ function updateStatus(text){
 		
 		function registerVehicleListeners(){
 			updateStatus('Adding Listener Vehicle API');
+			vehicle.get('shift', handleGear, errorCB);
+			
 			vehicle.addEventListener('shift', handleGear, false);
 			updateStatus('Listener registered.');
+			
+			vehicle.get('tripcomputer', handleAverageData, errorCB);
+			vehicle.addEventListener('tripcomputer', handleAverageData, false);
+			
 			findGeolocation();
+			
+			
+			
 		}
 		
 		function registerGeoListener(){
 			var params = {};
+				geolocation.getCurrentPosition(handlePosition, errorCB, params);
 			ps = geolocation.watchPosition(handlePosition,errorCB, params);
 		}
+		
+		
+
+
 		
 	
 });
@@ -201,7 +335,64 @@ function errorCB(error){
 
 
 function handleGear(data){
-	$('#v-gear').html(data.gear)
+	pdcAppHandler(data);
+	
+	switch(data.gear){
+		//Neutral 11, Parking 10; Rear 0
+		case '0':
+			gear = 'R';
+			break;
+		case '10':
+			gear = 'P';
+			break;
+		case '11':
+			gear = 'N';
+			break;
+		default:
+			gear = data.gear;
+			break;
+	}
+		$('#v-gear').html(gear);
+		dataModel[0].defaultV = gear;
+	if(dataModel[0].customField != null){
+		$('#' + dataModel[0].customField).find('.value').html(gear);
+	}
+}
+
+function handleAverageData(data){
+	$('#v-avg-speed').html(data.averageSpeed1);
+	$('#v-consumption').html(data.averageConsumption1);
+	$('#v-distance').html(data.tripDistance);
+	$('#v-range').html(data.range);
+	$('#v-mileage').html(data.mileage);
+	
+	dataModel[8].defaultV = data.averageConsumption1;
+	dataModel[9].defaultV =data.averageSpeed1;
+	dataModel[10].defaultV =data.mileage;
+	dataModel[11].defaultV =data.tripDistance;
+	dataModel[12].defaultV =data.range;
+	
+	if(dataModel[8].customField != null){
+		$('#' + dataModel[8].customField).find('.value').html(data.averageConsumption1);
+		
+	}
+	if(dataModel[9].customField != null){
+		$('#' + dataModel[9].customField).find('.value').html(data.averageSpeed1);
+		
+	}
+	if(dataModel[10].customField != null){
+		$('#' + dataModel[10].customField).find('.value').html(data.mileage);
+		
+	}
+	if(dataModel[11].customField != null){
+		$('#' + dataModel[11].customField).find('.value').html(data.tripDistance);
+		
+	}
+	if(dataModel[12].customField != null){
+		$('#' + dataModel[12].customField).find('.value').html(data.range);
+		
+	}
+
 }
 
 function handlePosition(data){
@@ -210,10 +401,430 @@ function handlePosition(data){
 	marker.setPosition(uPos);
 	map.setCenter(uPos);
 
+	$('#v-lat').html(Math.floor(data.coords.latitude * 10000)/10000);
+	$('#v-lng').html(Math.floor(data.coords.longitude * 10000)/10000);
+	$('#v-alt').html(data.coords.altitude);
+	$('#v-heading').html(data.coords.heading);
+	$('#v-heading2').html(data.coords.heading);
 	$('#v-speed').html(data.coords.speed);
+	
+	
+	dataModel[5].defaultV = Math.floor(data.coords.latitude * 10000)/10000;
+	dataModel[6].defaultV = Math.floor(data.coords.longitude * 10000)/10000;
+	dataModel[7].defaultV = data.coords.altitude;
+	dataModel[2].defaultV = data.coords.heading;
+	dataModel[1].defaultV = data.coords.speed;
+	
+	if(dataModel[5].customField != null){
+		$('#' + dataModel[5].customField).find('.value').html(Math.floor(data.coords.latitude * 10000)/10000);
+		
+	}
+
+	if(dataModel[6].customField != null){
+		$('#' + dataModel[6].customField).find('.value').html(Math.floor(data.coords.longitude * 10000)/10000);
+	}
+
+	if(dataModel[7].customField != null){
+		$('#' + dataModel[7].customField).find('.value').html(data.coords.altitude);
+	}
+	if(dataModel[2].customField != null){
+		$('#' + dataModel[2].customField).find('.value').html(data.coords.heading);
+	}
+	if(dataModel[1].customField != null){
+		$('#' + dataModel[1].customField).find('.value').html(data.coords.speed);
+	}
+
+	
 	
 }
 
 function handleError(error){
 	//logMessage(error)
 }
+
+						function drawPdcBase(position){
+					
+					if(position == "parksensors-front"){
+						canvas = $('#pdcFront');
+						centerX =  200;
+						centerY =  75;
+						start = 250;
+						end = 290;
+						radius = 154;
+						width = 88;
+						
+					
+					}else if(position == "parksensors-rear"){
+						canvas = $('#pdcRear');
+						centerX = -100;
+						centerY = 75; 
+						start = 70;
+						end = 110;
+						radius = 154;
+						width = 88;
+					}
+									canvas.clearCanvas();
+					
+					canvas.drawArc({
+  						strokeStyle: "#6C8080",
+  						opacity: 0.4,
+  						strokeWidth: width,
+  						x: centerX, y: centerY,
+  						radius: radius,
+  						start: start, end: end
+					});
+					
+					
+				}
+				
+				
+				function drawPdcObstacles(params){
+					
+					position = params.position;
+					left = params.left;
+					midLeft = params.midLeft;
+					midRight = params.midRight;
+					right = params.right;
+					
+					
+					widthRed = 10;
+					widthYellow = 14;
+					widthGreen = 20;
+					
+					radiusGreen1 = 188;
+					radiusGreen2 = 168;
+					
+					radiusYellow1 = 151;
+					radiusYellow2 = 137;
+					
+					radiusRed1 = 125;
+					radiusRed2 = 115;
+					
+					green = "#51FF19"
+					yellow = "#F3FF17";
+					red = "#FF3E00";
+					
+					
+					if(position == "parksensors-front"){
+						canvas = $('#pdcFront');
+						centerX =  200;
+						centerY =  75;
+						
+						//250 -- 290					
+						leftStart = 250;
+						leftEnd = 260;
+					
+						midLeftStart = 260;
+						midLeftEnd = 270;
+					
+						midRightStart = 270;
+						midRightEnd = 280;
+					
+						rightStart = 280;
+						rightEnd = 290;
+
+					
+					
+					}else if(position == "parksensors-rear"){
+						canvas = $('#pdcRear');
+						centerX = -100;
+						centerY = 75; 
+
+						//70 -- 110					
+						leftStart = 70;
+						leftEnd = 80;
+					
+						midLeftStart = 80;
+						midLeftEnd = 90;
+					
+						midRightStart = 90;
+						midRightEnd = 100;
+					
+						rightStart = 100;
+						rightEnd = 110;
+
+
+
+					}
+					drawPdcBase(position);
+					
+					
+					//250 185  120 80 40 20
+					
+					//GREEN  ONE
+					///LEFT
+					if(left < 250){
+						canvas.drawArc({
+							strokeStyle: green,
+							strokeWidth: widthGreen,
+							x: centerX, y: centerY,
+							radius: radiusGreen1,
+							start: leftStart, end: leftEnd
+						});
+					}
+					///MIDLEFT
+					if(midLeft < 250){
+					canvas.drawArc({
+  						strokeStyle: green,
+  						strokeWidth: widthGreen,
+  						x: centerX, y: centerY,
+  						radius: radiusGreen1,
+  						start: midLeftStart, end: midLeftEnd
+					});	
+					}
+					if(midRight < 250){
+					///MIDRIGHT
+					canvas.drawArc({
+  						strokeStyle: green,
+  						strokeWidth: widthGreen,
+  						x: centerX, y: centerY,
+  						radius: radiusGreen1,
+  						start: midRightStart, end: midRightEnd
+					});
+					}
+					///RIGHT
+					if(right < 250){
+					canvas.drawArc({
+  						strokeStyle: green,
+  						strokeWidth: widthGreen,
+  						x: centerX, y: centerY,
+  						radius: radiusGreen1,
+  						start: rightStart, end: rightEnd
+					});
+					}
+					//GREEN TWO
+					///LEFT
+					//250 185  120 80 40 20
+					if(left < 185){
+					canvas.drawArc({
+  						strokeStyle: green,
+  						strokeWidth: widthGreen,
+  						x: centerX, y: centerY,
+  						radius: radiusGreen2,
+  						start: leftStart, end: leftEnd
+					});
+					}
+					///MIDLEFT
+					if(midLeft < 185){
+					canvas.drawArc({
+  						strokeStyle: green,
+  						strokeWidth: widthGreen,
+  						x: centerX, y: centerY,
+  						radius: radiusGreen2,
+  						start: midLeftStart, end: midLeftEnd
+					});		
+					}
+					///MIDRIGHT
+					if(midRight < 185){
+					canvas.drawArc({
+  						strokeStyle: green,
+  						strokeWidth: widthGreen,
+  						x: centerX, y: centerY,
+  						radius: radiusGreen2,
+  						start: midRightStart, end: midRightEnd
+					});					
+					}
+					///RIGHT
+					if(right < 185){					
+					canvas.drawArc({
+  						strokeStyle: green,
+  						strokeWidth: widthGreen,
+  						x: centerX, y: centerY,
+  						radius: radiusGreen2,
+  						start: rightStart, end: rightEnd
+					});
+					}
+					//YELLOW ONE
+					///LEFT
+					if(left < 120){
+					canvas.drawArc({
+  						strokeStyle: yellow,
+  						strokeWidth: widthYellow,
+  						x: centerX, y: centerY,
+  						radius: radiusYellow1,
+  						start: leftStart, end: leftEnd
+					});
+					}
+					///MIDLEFT
+					if(midLeft < 120){
+					canvas.drawArc({
+  						strokeStyle: yellow,
+  						strokeWidth: widthYellow,
+  						x: centerX, y: centerY,
+  						radius: radiusYellow1,
+  						start: midLeftStart, end: midLeftEnd
+					});		
+					}
+					///MIDRIGHT
+					if(midRight < 120){
+					canvas.drawArc({
+  						strokeStyle: yellow,
+  						strokeWidth: widthYellow,
+  						x: centerX, y: centerY,
+  						radius: radiusYellow1,
+  						start: midRightStart, end: midRightEnd
+					});			
+					}
+					if(right < 120){
+					///RIGHT
+					canvas.drawArc({
+  						strokeStyle: yellow,
+  						strokeWidth: widthYellow,
+  						x: centerX, y: centerY,
+  						radius: radiusYellow1,
+  						start: rightStart, end: rightEnd
+					});		
+					}
+					//YELLOW TWO
+					///LEFT
+					if(left < 80){
+					canvas.drawArc({
+  						strokeStyle: yellow,
+  						strokeWidth: widthYellow,
+  						x: centerX, y: centerY,
+  						radius: radiusYellow2,
+  						start: leftStart, end: leftEnd
+					});
+					}
+					///MIDLEFT
+					if(midLeft < 80){
+					canvas.drawArc({
+  						strokeStyle: yellow,
+  						strokeWidth: widthYellow,
+  						x: centerX, y: centerY,
+  						radius: radiusYellow2,
+  						start: midLeftStart, end: midLeftEnd
+					});		
+					}
+					///MIDRIGHT
+					if(midRight < 80){
+					canvas.drawArc({
+  						strokeStyle: yellow,
+  						strokeWidth: widthYellow,
+  						x: centerX, y: centerY,
+  						radius: radiusYellow2,
+  						start: midRightStart, end: midRightEnd
+					});			
+					}
+					///RIGHT
+					if(right < 80){
+					canvas.drawArc({
+  						strokeStyle: yellow,
+  						strokeWidth: widthYellow,
+  						x: centerX, y: centerY,
+  						radius: radiusYellow2,
+  						start: rightStart, end: rightEnd
+					});	
+					}
+					//RED ONE
+					///LEFT
+					if(left < 40){
+					canvas.drawArc({
+  						strokeStyle: red,
+  						strokeWidth: widthRed,
+  						x: centerX, y: centerY,
+  						radius: radiusRed1,
+  						start: leftStart, end: leftEnd
+					});
+					}
+					
+					///MIDLEFT
+					if(midLeft < 40){
+					canvas.drawArc({
+  						strokeStyle: red,
+  						strokeWidth: widthRed,
+  						x: centerX, y: centerY,
+  						radius: radiusRed1,
+  						start: midLeftStart, end: midLeftEnd
+					});		
+					}
+					///MIDRIGHT
+					if(midRight < 40){
+					canvas.drawArc({
+  						strokeStyle: red,
+  						strokeWidth: widthRed,
+  						x: centerX, y: centerY,
+  						radius: radiusRed1,
+  						start: midRightStart, end: midRightEnd
+					});			
+					}
+					///RIGHT
+					if(right < 40){
+					canvas.drawArc({
+  						strokeStyle: red,
+  						strokeWidth: widthRed,
+  						x: centerX, y: centerY,
+  						radius: radiusRed1,
+  						start: rightStart, end: rightEnd
+					});		
+					}
+					//RED TWO
+					///LEFT
+					if(left < 20){
+					canvas.drawArc({
+  						strokeStyle: red,
+  						strokeWidth: widthRed,
+  						x: centerX, y: centerY,
+  						radius: radiusRed2,
+  						start: leftStart, end: leftEnd
+					});
+					}
+					///MIDLEFT
+					if(midLeft < 20){
+					canvas.drawArc({
+  						strokeStyle: red,
+  						strokeWidth: widthRed,
+  						x: centerX, y: centerY,
+  						radius: radiusRed2,
+  						start: midLeftStart, end: midLeftEnd
+					});				
+					}
+					///MIDRIGHT
+					if(midRight < 20){
+					canvas.drawArc({
+  						strokeStyle: red,
+  						strokeWidth: widthRed,
+  						x: centerX, y: centerY,
+  						radius: radiusRed2,
+  						start: midRightStart, end: midRightEnd
+					});			
+					}
+					///RIGHT
+					if(right < 20){
+					canvas.drawArc({
+  						strokeStyle: red,
+  						strokeWidth: widthRed,
+  						x: centerX, y: centerY,
+  						radius: radiusRed2,
+  						start: rightStart, end: rightEnd
+					});		
+					}
+				}
+
+		
+				function pdcAppHandler(event){
+					//Neutral 11, Parking 10; Rear 0
+					if(event.gear == 0){
+						enablePdc();
+					}
+					else if(event.gear >= 2){
+						disablePdc();
+						
+					}
+					
+				}
+				function enablePdc(){
+					pdcEnabled = true;
+					$("#pdc").fadeIn(500);
+					drawPdcBase("parksensors-front");
+					drawPdcBase("parksensors-rear");
+					vehicle.addEventListener('parksensors-front',drawPdcObstacles,false);
+					vehicle.addEventListener('parksensors-rear',drawPdcObstacles,false);
+				}
+
+				function disablePdc(){
+					pdcEnabled = false;
+					$("#pdc").fadeOut(500);
+					vehicle.removeEventListener('parksensors-front',drawPdcObstacles,false);
+					vehicle.removeEventListener('parksensors-rear',drawPdcObstacles,false);
+				}
