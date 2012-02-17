96f82864f7e38136e26eed6fd8da6a73974953f1
Felix-Johannes Jendrusch <felix-johannes.jendrusch@fokus.fraunhofer.de>
2012-02-17 02:51:53 +0100

    webinosFS: Ultra dirty-coded demo functionality (most basic)

--- demo/startPzp.js
+++ demo/startPzp.js
@@ -61,7 +61,7 @@ var pzpModules = [
     {name: "file", param: {}},
     {name: "geolocation", param: {}},
     {name: "applauncher", param: {}},
-//    {name: "events", param: {}},
+    {name: "events", param: {}},
     {name: "sensors", param: {}},
     {name: "payment", param: {}},
     {name: "tv", param: {}},
