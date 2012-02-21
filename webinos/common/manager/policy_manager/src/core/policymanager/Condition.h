643b8501317cc9be3a59f085eb9c88258ba41e3b
Stefano Vercelli <stefano.vercelli@telecomitalia.it>
2012-02-21 11:52:35 +0100

    Licenses fixed.

--- webinos/common/manager/policy_manager/src/core/policymanager/Condition.h
+++ webinos/common/manager/policy_manager/src/core/policymanager/Condition.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright 2010 Telecom Italia SpA
- *  
+ *  Code contributed to the webinos project
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Copyright 2011 Telecom Italia SpA
+ * 
  ******************************************************************************/
 
 #ifndef CONDITION_H_
