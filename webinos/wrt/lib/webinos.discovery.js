2e5a5564dd1d06d0c27b801278e01a94ead4771f
Ziran Sun <ziran.sun@samsung.com>
2012-02-22 18:12:41 +0000

    Add listener for discovery RPC; reverse changes on rpc.js

--- webinos/wrt/lib/webinos.discovery.js
+++ webinos/wrt/lib/webinos.discovery.js
@@ -8,7 +8,6 @@
 	BluetoothManager.prototype = new WebinosService;
   
 	BluetoothManager.prototype.bindService = function (bindCB, serviceId) {
-		// actually there should be an auth check here or whatever, but we just always bind
 		this.findHRM = findHRM;
 		this.listenAttr = {};
 		this.listenerForHRM = listenerForHRM.bind(this);
@@ -18,23 +17,20 @@
 		};
 	}
 
-
-	function listenerForHRM(listener, options) {
+	//function listenerForHRM(listener, options) {
+	BluetoothManager.prototype.listenerForHRM(listener, options){
 		var rpc = webinos.rpcHandler.createRPC(this, "listenAttr.listenForHRM", [options]);
 		rpc.fromObjectRef = Math.floor(Math.random()*101); //random object ID	
 
 		// create a temporary webinos service on the browser
 		var callback = new RPCWebinosService({api:rpc.fromObjectRef});
 		callback.onEvent = function (obj) {
-			// we were called back, now invoke the given listener
 			listener(obj); 
 		};
 		webinos.rpcHandler.registerCallbackObject(callback);
 
-	//	webinos.rpcHandler.executeRPC(rpc);
-	 	webinos.rpcHandler.executeRPC(rpc, function(params) {
-		success(params);
-  	 });
+		webinos.rpcHandler.executeRPC(rpc);
+	
 	}
 
 	BluetoothManager.prototype.findHRM = function(data, success){
