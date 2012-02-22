c48775ecf73bace7913958427d7024a99712bf9e
Ziran Sun <ziran.sun@samsung.com>
2012-02-22 16:03:40 +0000

    discovery rpc changes - first temp

--- webinos/api/discovery/lib/webinos.discovery.rpc.js
+++ webinos/api/discovery/lib/webinos.discovery.rpc.js
@@ -11,6 +11,28 @@
 				displayName:'Bluetooth discovery manager',
 				description:'A simple bluetooth discovery manager'
 			});
+
+	// member attribute
+	//this.testAttr = "Hello Attribute";
+
+	// member attribute 
+	this.listenAttr = {};
+	
+	// custom get42 attribute
+	//this.blaa = typeof params !== 'undefined' ? params[0] : 42;
+	
+	// member function attached to listenAttr
+	this.listenAttr.listenForHRM = function(params, successCB, errorCB, objectRef){
+		console.log("listenerForHRM was invoked");
+		
+		// call the registered listener twice, delivering an object
+		for (var i=0; i<2; i++) {
+			// use RPC to deliver result
+			var rpc = rpcHandler.createRPC(objectRef, 'onEvent', {msg:i + " HRM"});
+			rpcHandler.executeRPC(rpc);
+		}
+	  };
+
 	}
 
 	BluetoothManager.prototype = new RPCWebinosService;
--- webinos/wrt/lib/webinos.discovery.js
+++ webinos/wrt/lib/webinos.discovery.js
@@ -7,6 +7,36 @@
 	
 	BluetoothManager.prototype = new WebinosService;
   
+	BluetoothManager.prototype.bindService = function (bindCB, serviceId) {
+		// actually there should be an auth check here or whatever, but we just always bind
+		this.findHRM = findHRM;
+		this.listenAttr = {};
+		this.listenerForHRM = listenerForHRM.bind(this);
+		
+		if (typeof bindCB.onBind === 'function') {
+			bindCB.onBind(this);
+		};
+	}
+
+
+	function listenerForHRM(listener, options) {
+		var rpc = webinos.rpcHandler.createRPC(this, "listenAttr.listenForHRM", [options]);
+		rpc.fromObjectRef = Math.floor(Math.random()*101); //random object ID	
+
+		// create a temporary webinos service on the browser
+		var callback = new RPCWebinosService({api:rpc.fromObjectRef});
+		callback.onEvent = function (obj) {
+			// we were called back, now invoke the given listener
+			listener(obj); 
+		};
+		webinos.rpcHandler.registerCallbackObject(callback);
+
+	//	webinos.rpcHandler.executeRPC(rpc);
+	 	webinos.rpcHandler.executeRPC(rpc, function(params) {
+		success(params);
+  	 });
+	}
+
 	BluetoothManager.prototype.findHRM = function(data, success){
 		console.log("HRM find HRM");
   		var rpc = webinos.rpcHandler.createRPC(this, "findHRM",arguments);
