e0578c319ccab1fdc0f95acc3f6c62e063c814fe
Cesare Cameroni <cesare.cameroni@polito.it>
2012-02-22 10:53:03 +0100

    License header modification in session_schema.js

--- webinos/pzp/lib/session_schema.js
+++ webinos/pzp/lib/session_schema.js
@@ -13,7 +13,8 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *
-* Copyright 2011 POLITO
+* Copyright 2011 Torsec -Computer and network security group-
+* Politecnico di Torino
 *******************************************************************************/
 
 
