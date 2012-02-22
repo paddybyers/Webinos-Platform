3fc02cb5dd3ed7c651b882d683d4f776e210bfbd
Christian Fuhrhop <christian.fuhrhop@fokus.fraunhofer.de>
2012-02-22 15:09:23 +0100

    Added licence headers

--- webinos/api/payment/lib/gsma_payment.js
+++ webinos/api/payment/lib/gsma_payment.js
@@ -1,4 +1,23 @@
-    /**
+   /*******************************************************************************
+    *  Code contributed to the webinos project
+    *
+    * Licensed under the Apache License, Version 2.0 (the "License");
+    * you may not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    *
+    *     http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+    *
+    * Copyright 2012 Christian Fuhrhop, Fraunhofer FOKUS
+    * 
+    ******************************************************************************/ 
+    
+      /**
      * The Payment connector to GSMA Payment API
      *
      */
@@ -68,7 +87,7 @@
         
        console.log("DATA: " + chunk);
        // do we have the subscriber in the reply?
-       reply=""+chunk;	
+       reply=""+chunk;  
        if(reply.indexOf("<error>")!=-1)retcode=GSMA_Error_Extract(reply,"<error>","</error>");
        else if(reply.indexOf("401 - Not Authorized")!=-1)retcode="Shop not authorized";
        else retcode="ok";
@@ -76,7 +95,7 @@
     });
     response.on("close", function (err) {
             error.code = 1;
-            error.message = "Unexpected close on call to payment server";	
+            error.message = "Unexpected close on call to payment server";       
             errorCallback(error); 
     });
 
@@ -183,7 +202,7 @@
     });
     response.on("close", function (err) {
             error.code = 1;
-            error.message = "Unexpected close on call to payment server";	
+            error.message = "Unexpected close on call to payment server";       
             errorCallback(error); 
     });
 
@@ -273,7 +292,7 @@
     });
     response.on("close", function (err) {
             error.code = 1;
-            error.message = "Unexpected close on call to payment server";	
+            error.message = "Unexpected close on call to payment server";       
             errorCallback(error); 
     });
 
