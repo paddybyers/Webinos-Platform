3467be8c578138ad9d92d5cf46d5a58d99d1ef02
Paddy Byers <paddy.byers@gmail.com>
2012-02-23 07:45:07 +0000

    Add injection of webinos.js

--- webinos/common/android/wrt/src/org/webinos/wrt/renderer/WebChromeClient.java
+++ webinos/common/android/wrt/src/org/webinos/wrt/renderer/WebChromeClient.java
@@ -1,5 +1,7 @@
 package org.webinos.wrt.renderer;
 
+import org.webinos.wrt.ui.RendererActivity;
+
 import android.app.AlertDialog;
 import android.util.Log;
 import android.webkit.JsResult;
@@ -7,7 +9,13 @@ import android.webkit.WebView;
 
 public class WebChromeClient extends android.webkit.WebChromeClient{
 
+	private RendererActivity activity;
+
 	private static final String TAG = "org.webinos.wrt.renderer.WebChromeClient";
+
+	public WebChromeClient(RendererActivity activity) {
+		this.activity = activity;
+	}
 
 	@Override
     public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
@@ -16,5 +24,11 @@ public class WebChromeClient extends android.webkit.WebChromeClient{
         result.confirm();
         return true;
     }
+	
+	@Override
+	public void onReceivedTitle(WebView webView, String title) {
+		if(title != null && !title.isEmpty())
+			activity.setTitle(title);
+	}
 
 }
